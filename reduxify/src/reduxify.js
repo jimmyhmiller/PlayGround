d4f4928ddc6f0f22ef6f91f119eba6994cacbecb
Jimmy Miller <jimmyhmiller@gmail.com>
2016-12-08 01:34:16 -0500

    code clean up

--- reduxify/src/reduxify.js
+++ reduxify/src/reduxify.js
@@ -10,6 +10,24 @@ const difference = function(setA, setB) {
     return temp;
 }
 
+
+const printWarningMessage = (schema, values, type) => {
+  const schemaSet = new Set(keys(schema));
+  const valuesSet = new Set(keys(values));
+  const missingKeys = difference(schemaSet, valuesSet);
+  const extraKeys = difference(valuesSet, schemaSet);
+  if (extraKeys.size > 0 && missingKeys.size > 0) {
+      console.warn(`Action of type ${type} is missing the following key(s): [${Array.from(missingKeys.values()).join(", ")}]\nIt also has the following extra key(s): [${Array.from(extraKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
+  }
+  else if (missingKeys.size > 0) {
+      console.warn(`Action of type ${type} is missing the following key(s): [${Array.from(missingKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
+  }
+  else if (extraKeys.size > 0) {
+      console.warn(`Action of type ${type} has the following extra key(s): [${Array.from(extraKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
+  }
+}
+
+
 export const reducers = (red, ...reds) => (state, action) => 
   reds.reduce((state, f) => f(state, action), red(state, action))
 
@@ -17,24 +35,13 @@ export const initial = initialState => state => state ? state : initialState;
 
 export const action = ({ type, ...schema }) => {
     const actionCreator = (values) => {
-        const schemaSet = new Set(keys(schema));
-        const valuesSet = new Set(keys(values));
-        const missingKeys = difference(schemaSet, valuesSet);
-        const extraKeys = difference(valuesSet, schemaSet);
-        if (extraKeys.size > 0 && missingKeys.size > 0) {
-            console.warn(`Action of type ${type} is missing the following key(s): [${Array.from(missingKeys.values()).join(", ")}]\nIt also has the following extra key(s): [${Array.from(extraKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
-        }
-        else if (missingKeys.size > 0) {
-            console.warn(`Action of type ${type} is missing the following key(s): [${Array.from(missingKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
-        }
-        else if (extraKeys.size > 0) {
-            console.warn(`Action of type ${type} has the following extra key(s): [${Array.from(extraKeys.values()).join(", ")}]\nThe action creator was given the following object:\n${JSON.stringify(values, null, '  ')}`)
-        }
+        printWarningMessage(schema, values, type);
         return {
             type,
             ...values,
         }
     }
+
     actionCreator.reduce = f => (state, action) => {
       if (action && action.type === type) {
         return f(state,action)
