76e0184f28a52079ae40cc35211845180fabaf40
Jimmy Miller <jimmyhmiller@gmail.com>
2017-11-19 21:15:01 -0500

    Messy but working macros

--- experiment-spec/src/experiment_spec/interpret.clj
+++ experiment-spec/src/experiment_spec/interpret.clj
@@ -3,6 +3,7 @@
 
 (defn convert-match [case]
   (core.match/match [case]
+                    [else :guard keyword?] else
                     [[sym pred]] [sym :guard pred]
                     [s-expr] [(list (into [] s-expr) :seq)]))
 
@@ -10,10 +11,13 @@
   `(core.match/match ~c ~@(mapcat (fn [[k v]] [(convert-match k) v]) (partition 2 cases))))
 
 
-(defn env [] (atom {'+ + '= = '* * '- - 'println println}))
+(defn env 
+  ([] (env {}))
+  ([extend] (atom (merge {'+ + '= = '* * '- - 'println println 'inc inc} extend))))
 
 (defn macro? [env n]
-  (-> @env n :meta :macro))
+  (and (symbol? n)
+       (-> @env n :meta :macro)))
 
 (defn get-var [env n] 
   (let [var (@env n)]
@@ -27,36 +31,73 @@
    env))
 
 
-(defmacro const [x] 2)
+(defn add-vars [env names vals]
+  (reduce 
+   (fn [env [name val]] 
+     (add-var env name val))
+   env 
+   (map vector names vals)))
 
-(const ((fn [x] 2)))
+(defmacro const [f] `(~f 2))
+
+(defmacro let-it [arg body]
+  `((fn [~(first arg)] ~body) ~(second arg)))
+
+
+(def eval-expr)
+(def macro-expansion)
+
+(defn expand-form [env expr]
+  (match [expr]
+         ('unquote x) (eval-expr x env)
+         :else expr))
+
+(defn macro-expansion [expr env]
+  (clojure.walk/postwalk (partial expand-form env) expr))
 
 
 (defn eval-expr [expr env]
   (match [expr]
-         [n nil?] nil
+         [n nil?]                       nil
          [n boolean?]                   n
          [n number?]                    n
          [n symbol?]                    (get-var env n)
          ('quote x)                     x
-         ('defmacro name [x] body)      (add-var env name 
-                                                 (fn [arg] (eval-expr body (add-var env x arg)))
+         ('defmacro name 
+          [& names] body)               (add-var env name 
+                                                       (fn [& vals] 
+                                                         (eval-expr 
+                                                          (macro-expansion body 
+                                                                           (add-vars env names vals)) env))
                                                  {:macro true})
          ('def name x)                  (add-var env name (eval-expr x env))
          ('if pred t f)                 (if (eval-expr pred env)
                                           (eval-expr t env)
                                           (eval-expr f env))
          ('do & exprs)                  (last (doall (map #(eval-expr % env) exprs)))
-         ('fn [x] body)                 (fn [arg] (eval-expr body (add-var env x arg)))
-         (f arg)                        (if (macro? env f)
-                                          (eval-expr ((eval-expr f env) arg) env)
-                                          ((eval-expr f env) (eval-expr arg env)))))
+         ('fn [& names] body)          (fn [& vals] (eval-expr body (add-vars env names vals)))
+         (f & args)                       (if (macro? env f)
+                                            (eval-expr (apply (eval-expr f env) args) env)
+                                            (apply (eval-expr f env) (map #(eval-expr % env) args)))))
+
+
+
+
+
+(def my-env (env))
+
+(def core-library
+  '(do
+     (defmacro defn [name args body] 
+       (quote (def (unquote name) (fn (unquote args) (unquote body)))))))
+
+(eval-expr core-library my-env)
 
 (eval-expr 
- '(do (defmacro debugging [f] 
-        (f 2))
-      (debugging (fn [y] y))) (env))
-
+ '(do 
+     (defn f [x y] (+ x y))
+     (f 2 3)) my-env)
+    
 
 (comment
   (defn eval-expr [expr env]
