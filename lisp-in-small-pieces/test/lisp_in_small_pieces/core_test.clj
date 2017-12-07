8fc7e1f2375964459058b3d7108eca0f0dfa8843
Jimmy Miller <jimmyhmiller@gmail.com>
2017-12-07 00:53:56 -0500

    start of lisp-in-small-pieces

---
+++ lisp-in-small-pieces/test/lisp_in_small_pieces/core_test.clj
@@ -0,0 +1,7 @@
+(ns lisp-in-small-pieces.core-test
+  (:require [clojure.test :refer :all]
+            [lisp-in-small-pieces.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
