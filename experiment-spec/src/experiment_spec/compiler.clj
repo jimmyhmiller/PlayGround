6e01112e678c4fa8c8383364a9ab6bb725ef14af
Jimmy Miller <jimmyhmiller@gmail.com>
2017-12-17 14:20:47 -0500

    forgot to add things

---
+++ experiment-spec/src/experiment_spec/compiler.clj
@@ -0,0 +1,113 @@
+(ns experiment-spec.compiler
+  (:require [clojure.core.match :as core.match]))
+
+
+;http://matt.might.net/articles/compiling-up-to-lambda-calculus/
+
+(defn convert-match [case]
+  (core.match/match [case]
+                    [else :guard keyword?] else
+                    [[sym pred]] [sym :guard pred]
+                    [s-expr] [(list (into [] s-expr) :seq)]))
+
+(defmacro match [c & cases]
+  `(core.match/match ~c ~@(mapcat (fn [[k v]] [(convert-match k) v]) (partition 2 cases))))
+
+(def VOID `(fn [void#] void#))
+
+
+
+(def ERROR `(fn [_]
+              ((fn [f#] (f# f#)) (fn [f#] (f# f#)))))
+
+(def TRUE `(fn [t#] (fn [f#] (t# ~VOID))))
+(def FALSE `(fn [t#] (fn [f#] (f# ~VOID))))
+
+
+(def apply-n 
+  (fn [f n z]
+    (cond 
+      (= n 0) z
+      :else `(~f ~(apply-n f (- n 1) z)))))
+
+(defn church-numerals [n]
+  (cond
+    (= n 0) `(fn [f#] (fn [z#] z#))
+    :else   `(fn [f#] (fn [z#]
+                       ~(apply-n 'f# n 'z#)))))
+
+(def ZERO? (fn [n#]
+             ((n# (fn [_#] ~FALSE)) ~TRUE)))
+
+(def SUM '(fn [n#]
+            (fn [m#]
+              (fn [f#]
+                (fn [z#]
+                  ((m# f#) ((n# f#) z#)))))))
+
+(def MUL '(fn [n#]
+            (fn [m#]
+              (fn [f#]
+                (fn [z#]
+                  ((m# (n# f#)) z#))))))
+
+(def PRED '(fn [n#]
+             (fn [f#]
+               (fn [z#]
+                 (((n# (fn [g#] (fn [h#] (h# (g# f#)))))
+                   (fn [u#] z#))
+                  (fn [u#] u#))))))
+
+(def SUB `(fn [n#]
+            (fn [m#]
+              ((m# ~PRED) n#))))
+
+(def CONS `(fn [car#]
+             (fn [cdr#]
+               (fn [on-cons#]
+                 (fn [on-nil#]
+                   ((on-cons# car#) cdr#))))))
+
+(def NIL `(fn [on-cons#]
+            (fn [on-nil#]
+              (on-nil# ~VOID))))
+
+(def CAR `(fn [list#]
+            ((list# (fn [car#]
+                     (fn [cdr#]
+                       (car#))))
+             ~ERROR)))
+
+(def CDR `(fn [list#]
+            ((list# (fn [car#]
+                     (fn [cdr#]
+                       cdr#)))
+             ~ERROR)))
+
+(def PAIR? `(fn [list#]
+              ((list# (fn [_#] (fn [_#] ~TRUE)))
+               (fn [_] ~FALSE))))
+
+(def NULL? `(fn [list#]
+              ((list# (fn [_#] (fn [_#] ~FALSE)))
+               (fn [_#] ~TRUE))))
+
+(def Y ((fn [y#] (fn [F#] (F# (fn [x#] (((y# y#) F#) x#)))))
+        (fn [y#] (fn [F#] (F# (fn [x#] (((y# y#) F#) x#)))))))
+
+
+(defn compile [expr]
+  (match [expr]
+         [_ (partial = true)] TRUE
+         [_ (partial = false)] FALSE
+         [_ symbol?] expr
+         ('if cond t f) (compile `(~cond (fn [] ~t) (fn [] ~f)))
+         (fn [] exp) `(fn [_#] ~(compile exp))
+         (fn [v] exp) `(fn [~v] ~(compile exp))
+         (f) (compile `(~(compile f) ~VOID))
+         (f exp) `(~(compile f) ~(compile exp))
+         (f exp1 exp2) (compile `((~f ~exp1) ~exp2))))
+
+
+
+
