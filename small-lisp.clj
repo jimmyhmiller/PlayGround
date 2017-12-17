e85870af2719a60a9e8f94b3058ce9602c6f99eb
Jimmy Miller <jimmyhmiller@gmail.com>
2017-12-17 15:35:00 -0500

    fix code order

--- small-lisp.clj
+++ small-lisp.clj
@@ -60,6 +60,25 @@
   (fn [values]
     (eprogn body (extend env (zipmap variables values)))))
 
+
+(defn eval [e env]
+  (if (atom? e)
+    (cond
+      (symbol? e) (get @env e)
+      (or (number? e) (string? e) (char? e) (boolean? e)) e
+      :else (error "cannot evaluate " e))
+    (case (first e)
+      quote (second e)
+      if (if (eval (nth e 1) env)
+           (eval (nth e 2) env)
+           (eval (nth e 3) env))
+      begin (eprogn (rest e) env)
+      set! (update! (nth e 1) env (eval (nth e 2) env))
+      lambda (make-function (second e) (nthrest e 2) env)
+      (invoke (eval (first e) env)
+              (evlis (rest e) env)))))
+
+
 (eval 1 {})
 (eval \s {})
 (eval true {})
@@ -70,7 +89,7 @@
 (eval '(if false 0 1) {})
 (eval '(if a 0 1) (create-env {'a true}))
 (eval '(if a 0 1) (create-env {'a false}))
-(eval '(begin 1 2 3) {})
+(eval '(begin 1 2 3) (create-env {}))
 (eval '(begin (set! a 2) a) (create-env {'a 3}))
 (eval '((lambda (f) (f)) (lambda (x) 2))
       (create-env {'list (partial apply list)}))
@@ -103,14 +122,13 @@
 
 
 (defmulti eval' (fn [e env] (atom? e)))
+(defmulti eval-atom (fn [e env] (symbol? e)))
+(defmulti eval-list (fn [e env] (first e)))
+
 
 
 (defmethod eval' true [e env] (eval-atom e env))
 (defmethod eval' false [e env] (eval-list e env))
-
-
-(defmulti eval-atom (fn [e env] (symbol? e)))
-(defmulti eval-list (fn [e env] (first e)))
 
 
 
@@ -129,7 +147,6 @@
 (defmethod eval-list 'quote [[_ body] env]
   (eval' body env))
 
-
 ;if
 (defmethod eval-list 'if [[_ pred true-case false-case] env]
   (if (eval' pred env)
@@ -140,8 +157,6 @@
 ;begin
 (defmethod eval-list 'begin [[_ & body] env]
   (eprogn' body env))
-
-
 
 ;set!
 (defmethod eval-list 'set! [[_ var value] env]
@@ -171,19 +186,3 @@
 
 
 
-(defn eval [e env]
-  (if (atom? e)
-    (cond
-     (symbol? e) (get @env e)
-     (or (number? e) (string? e) (char? e) (boolean? e)) e
-     :else (error "cannot evaluate " e))
-    (case (first e)
-      quote (second e)
-      if (if (eval (nth e 1) env)
-           (eval (nth e 2) env)
-           (eval (nth e 3) env))
-      begin (eprogn (rest e) env)
-      set! (update! (nth e 1) env (eval (nth e 2) env))
-      lambda (make-function (second e) (nthrest e 2) env)
-      (invoke (eval (first e) env)
-              (evlis (rest e) env)))))
