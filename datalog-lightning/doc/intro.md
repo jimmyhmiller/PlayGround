e72e7117346b2bb05b4d38e194f2cc0abe9543b6
Jimmy Miller <jimmyhmiller@gmail.com>
2018-03-11 11:52:15 -0400

    Changes

---
+++ datalog-lightning/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datalog-lightning
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ datalog-lightning/test/datalog_lightning/core_test.clj
@@ -0,0 +1,7 @@
+(ns datalog-lightning.core-test
+  (:require [clojure.test :refer :all]
+            [datalog-lightning.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
