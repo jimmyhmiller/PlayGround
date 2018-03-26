a05ebc1695e06d6c86b058c11d53b6075e0ff38c
Jimmy Miller <jimmyhmiller@gmail.com>
2018-03-26 11:31:13 -0400

    one hundred

---
+++ one-hundred/100 Lines or Less.md
@@ -0,0 +1,21 @@
+# 100 Lines or Less
+
+* Virtual Dom
+* Redux
+* Redux Saga
+* Lisp Interpreter
+* Lambda Compiler
+* Pattern Matcher
+* Object System
+* Type Inference
+* Neural Network
+* Parser
+* Type Safe Printf
+* Various Monads
+* Purely Functional Data Structures
+* Promises
+* Multi-methods
+* Logic Normalizer
+* Finite State Machine
+* Flask
+* Service Discovery
---
+++ one-hundred/redux.js
@@ -0,0 +1,45 @@
+const counter = (state=0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': {
+            return state + 1;
+        }
+        case 'DECREMENT': {
+            return state - 1;
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+
+const createStore = (reducer) => {
+    let state = reducer(undefined, {});
+    let listeners = [];
+    return {
+        subscribe: (f) => { 
+            listeners.push(f);
+            const fsPosition = listeners.length - 1 
+            return () => listeners.splice(fsPosition, 1);
+        },
+        getState: () => state,
+        dispatch: (action) => {
+            state = reducer(state, action)
+            listeners.forEach(l => l());
+        },
+    }
+}
+
+
+const store = createStore(counter)
+
+const unsub = store.subscribe(() => console.log(store.getState()))
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'DECREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+unsub()
+store.dispatch({type: 'INCREMENT'})
+console.log('\n\n')
