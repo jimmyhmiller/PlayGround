9d3b49386bfec15cbf62296068e9121ae9669286
Jimmy Miller <jimmyhmiller@gmail.com>
2018-06-03 01:20:35 -0400

    Changes

--- presentations/generative-testing-elixir/src/Presentation.js
+++ presentations/generative-testing-elixir/src/Presentation.js
@@ -50,15 +50,61 @@ export default () =>
       text="Don't write tests"
       subtext="Generate them" />
 
+    <Points title="Outline">
+      <Point text="Unit Tests" />
+      <Point text="Elixir's Property-Based Testing" />
+      <Point text="Examples and Potential Future" />
+      <Point text="Details and Other Communities" />
+    </Points>
+
+
+    <Points title="What is a Unit Test">
+      <Point text="Tests a (typically) small chunk of code" />
+      <Point text="Programmer creates examples" />
+      <Point text="Asserts output is as expected" />
+    </Points>
+
+    <Code
+      title="Unit Test Example"
+      lang="elixir"
+      source={`
+        test "Test Reverse" do
+          assert reverse([]) == []
+          assert reverse([1]) == [1]
+          assert reverse([1,2,3]) == [3,2,1]
+          assert reverse(0..10) == Enum.to_list 10..0
+        end
+      `} />
+
     <Points title="Unit Test Problems">
       <Point text="Lots of code" />
       <Point text="Don't test whole space" />
       <Point text="Tend towards testing implementation details" />
     </Points>
 
+    <Code
+      title="Property-Based Test Example"
+      lang="elixir"
+      source={`
+        property "Reverse a reverse doesn't change" do
+          check all list <- list_of(integer()) do
+            assert reverse(reverse(list)) == list
+          end
+        end
+      `} />
 
-
-
+    <Code
+      title="Property-Based Test Example"
+      lang="elixir"
+      source={`
+        property "Reverse append is append reverse" do
+          check all list1 <- list_of(integer()),
+                    list2 <- list_of(integer()) do
+            assert reverse(list1 ++ list2) == 
+                   reverse(list2) ++ reverse(list1)
+          end
+        end
+      `} />
 
 
     <BlankSlide />
