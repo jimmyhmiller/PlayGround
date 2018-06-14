3b659392540e5f0b01e08b7ee79ea07e24a9560b
Jimmy Miller <jimmyhmiller@gmail.com>
2018-06-13 21:01:28 -0400

    changes

--- one-hundred/redux.js
+++ one-hundred/redux.js
@@ -1,20 +1,26 @@
-const counter = (state=0, action) => {
-    switch (action.type) {
-        case 'INCREMENT': {
-            return state + 1;
-        }
-        case 'DECREMENT': {
-            return state - 1;
-        }
-        default: {
-            return state
-        }
-    }
+console.log('\033c')
+
+
+const compose = (f, g) => (x) => f(g(x))
+
+const applyMiddleware = (...middlewares) => (createStore) => (...args) => {
+    const store = createStore(...args)
+    const dispatch = middlewares
+        .map(m => m(store))
+        .reduce(compose)(store.dispatch)
+    return Object.assign(
+        {},
+        store,
+        {dispatch: dispatch}
+    )
 }
 
 
-const createStore = (reducer) => {
-    let state = reducer(undefined, {});
+const createStore = (reducer, initialState, enhancer) => {
+    if (enhancer) {
+        return enhancer(createStore)(reducer, initialState)
+    }
+    let state = reducer(initialState, {});
     let listeners = [];
     return {
         subscribe: (f) => { 
@@ -30,8 +36,35 @@ const createStore = (reducer) => {
     }
 }
 
+/////////////////////////////////////////////////
 
-const store = createStore(counter)
+const counter = (state=0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': {
+            return state + 1;
+        }
+        case 'DECREMENT': {
+            return state - 1;
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+function logger({ getState }) {
+  return next => action => {
+    console.log('will dispatch', action)
+
+    const returnValue = next(action)
+    console.log('state after dispatch', getState())
+
+    return returnValue
+  }
+}
+
+
+const store = createStore(counter, 12, applyMiddleware(logger))
 
 const unsub = store.subscribe(() => console.log(store.getState()))
 store.dispatch({type: 'INCREMENT'})
@@ -42,4 +75,3 @@ store.dispatch({type: 'INCREMENT'})
 store.dispatch({type: 'INCREMENT'})
 unsub()
 store.dispatch({type: 'INCREMENT'})
-console.log('\n\n')
