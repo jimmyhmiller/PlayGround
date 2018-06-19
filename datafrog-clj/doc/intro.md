8746c0ad7d6627a22acc4e1897d2e71810ebc26a
Jimmy Miller <jimmyhmiller@gmail.com>
2018-06-19 01:05:11 -0400

    Changes

---
+++ datafrog-clj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to datafrog-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ datafrog-clj/test/datafrog_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns datafrog-clj.core-test
+  (:require [clojure.test :refer :all]
+            [datafrog-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
