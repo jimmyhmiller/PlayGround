02f7cc67d455b68464c97588f1fe7e6e40ba2c7c
Jimmy Miller <jimmyhmiller@gmail.com>
2018-08-09 23:42:17 -0400

    Changes

--- critaeus/standup/minimal-api/index.js
+++ critaeus/standup/minimal-api/index.js
@@ -1,8 +1,60 @@
-const { router, get } = require('microrouter')
+const { send, json } = require('micro')
+const { router, get, post } = require('microrouter')
+const s3 = new (require('aws-sdk/clients/s3'))();
 
-const welcome = (req, res) => {
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400')
-    res.end('Welcome to Micro')
+const bucket = 'a-bucket-that-i-dont-care-about';
+
+const writeToS3 = (bucket, key, data) => {
+  return s3.putObject({
+    Bucket: bucket,
+    Key: key,
+    Body: JSON.stringify(data),
+  }).promise()
 }
 
-module.exports = router(get('/welcome', welcome))
+const readFromS3 = (bucket, key) => {
+  return s3.getObject({
+    Bucket: bucket,
+    Key: key,
+  })
+  .promise()
+  .then(data => JSON.parse(data.Body.toString()))
+}
+
+
+const handleKeyNotFound = (e, cb) => {
+  if (e.code === "NoSuchKey") {
+    cb(400, {error: 'Status not found'})
+  } else {
+    console.log(e)
+    cb(500, {error: 'Unexpected error'})
+  }
+}
+
+const getDatesStatus = async (req, res) => {
+  try {
+    const { team, user, date } = req.params;
+    const data = await readFromS3(bucket, `${team}/${date}/${user}.json`)
+    send(res, 200, data)
+  } catch (e) {
+    handleKeyNotFound(e, send.bind(null, res))
+  }
+}
+
+const postDatesStatus = async (req, res) => {
+  try {
+    const body = await json(req)
+    const { team, user, date } = req.params;
+    await writeToS3(bucket, `${team}/${date}/${user}.json`, body)
+    send(res, 201)
+  } catch (e) {
+    console.log(e)
+    send(res, 500, e)
+  }
+}
+
+
+module.exports = router(
+  get('/standup/:team/:date/:user', getDatesStatus),
+  post('/standup/:team/:date/:user', postDatesStatus),
+)
