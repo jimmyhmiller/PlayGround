d72c366b9ff9baf92ef7078433e1877818fa1362
Jimmy Miller <jimmyhmiller@gmail.com>
2018-08-26 13:29:29 -0400

    Remove files

--- devcard/resources/public/js/compiled/devcards_out/figwheel/connect.cljs
+++
@@ -1,4 +0,0 @@
-(ns figwheel.connect (:require [devcards.core :include-macros true] [devcard.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:build-id "devcards", :devcards true, :websocket-url "ws://localhost:3449/figwheel-ws"})
-(devcards.core/start-devcard-ui!)
-
