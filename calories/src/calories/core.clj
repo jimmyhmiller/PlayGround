99b67975f3e3a928f4798cfbd83c835304db10f8
Jimmy Miller <jimmyhmiller@gmail.com>
2018-08-27 01:22:20 -0400

    Changes

---
+++ calories/src/calories/core.clj
@@ -0,0 +1,32 @@
+(ns calories.core
+  (:require [cheshire.core :as json])
+  (:import (java.text SimpleDateFormat))
+  (:gen-class))
+
+(def file-path "/Users/jimmy/.calories.json")
+
+(defn write-file [file content]
+  (spit file (json/generate-string content)))
+
+(defn read-file [file]
+  (try 
+    (json/parse-string (slurp file) true)
+    (catch Exception e
+      (write-file file []))))
+
+(defn today []
+  (.format
+   (SimpleDateFormat. "yyyy-MM-dd")
+   (java.util.Date.)))
+
+(defn make-record [date calories]
+  {:date date
+   :calories calories})
+
+(defn add-record [file calories]
+  (write-file file (conj (read-file file) (make-record (today) calories))))
+
+(defn -main [calories]
+  (add-record file-path (Integer/parseInt calories))
+  (println "Record added"))
+
