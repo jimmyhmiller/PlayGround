fe2d9824e9ca4a51a10bd9184e6b7be6a0970b4a
Jimmy Miller <jimmyhmiller@gmail.com>
2018-08-28 22:24:54 -0400

    Add multiple method and dry-run ability

--- calories/src/calories/core.clj
+++ calories/src/calories/core.clj
@@ -1,25 +1,39 @@
 (ns calories.core
   (:require [cheshire.core :as json])
-  (:import (java.text SimpleDateFormat))
+  (:import (java.time LocalDate)
+           (java.time.format DateTimeFormatter))
   (:gen-class))
 
-(def file-path "/Users/jimmy/.calories.json")
 
+(def ^:dynamic dry-run false)
+
+(def file-path (str (System/getProperty "user.home") "/.calories.json"))
+
+(def pound 3500)
+(def base-rate 2500)
 (def daily 1500)
 
 (defn write-file [file content]
-  (spit file (json/generate-string content)))
+  (if dry-run
+    (clojure.pprint/pprint content)
+    (spit file (json/generate-string content))))
 
 (defn read-file [file]
-  (try 
+  (try
     (json/parse-string (slurp file) true)
     (catch Exception e
       (write-file file []))))
 
-(defn today []
+(defn format-date [local-date]
   (.format
-   (SimpleDateFormat. "yyyy-MM-dd")
-   (java.util.Date.)))
+   local-date
+   DateTimeFormatter/ISO_LOCAL_DATE))
+
+(defn today []
+  (format-date (LocalDate/now)))
+
+(defn yesterday []
+  (format-date (.minusDays (LocalDate/now) 1)))
 
 (defn make-record [date calories]
   {:date date
@@ -28,21 +42,34 @@
 (defn add-record [file calories]
   (write-file file (conj (read-file file) (make-record (today) calories))))
 
-(defn total-today []
+(defn total [date]
   (->> file-path
        read-file
-       (filter (comp #{(today)} :date))
+       (filter (comp #{date} :date))
        (map :calories)
        (reduce + 0)))
 
-(defn calories-left []
-  (- daily (total-today)))
+(defn calories-left [date]
+  (- daily (total date)))
+
+(defmulti command (comp keyword first))
+
+(defmethod command :add [[_ calories]]
+  (add-record file-path (Integer/parseInt calories))
+  (println (str "Added " calories " calories"))
+  (println (str (calories-left (today)) " calories left")))
+
+(defmethod command :today [_]
+  (println (str (calories-left (today)) " calories left")))
+
+(defmethod command :yesterday [_]
+  (println (str (calories-left (yesterday)) " calories left")))
+
+(defmethod command :default [args]
+  (if-let [calories (first args)]
+    (command [:add calories])
+    (command [:today])))
 
 (defn -main [& args]
-  (let [calories (first args)]
-    (if calories
-      (do
-        (add-record file-path (Integer/parseInt calories))
-        (println (str "Added " calories " calories"))
-        (println (str (calories-left) " calories left")))
-      (println (total-today)))))
+  (command args))
+
