50023540c0ce27b112544fcd7ab6855258a13ed9
Jimmy Miller <jimmyhmiller@gmail.com>
2018-09-12 10:05:18 -0400

    Changes

--- rust/wolf/src/main.rs
+++ rust/wolf/src/main.rs
@@ -1,9 +1,10 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use sdl2::rect::Point;
+use sdl2::rect::Point as SdlPoint;
 use sdl2::render::Canvas;
 use std::collections::HashSet;
+use std::ops::Add;
 
 use sdl2::video::Window;
 
@@ -15,6 +16,60 @@ fn clear_canvas(canvas: &mut Canvas<Window>) {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     // fills the canvas with the color we set in `set_draw_color`.
     canvas.clear();
+}
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+type Camera = Point;
+type Facing = Point;
+type Position = Point;
+type PositionDelta = Point;
+
+struct Player {
+    facing: Facing,
+    position: Position,
+    camera: Camera,
+    rotation_speed: f64,
+    move_speed: f64,
+}
+
+fn rotate_point(point: &Point, rotation_speed: f64) -> Point {
+    Point {
+        x: point.x * rotation_speed.cos() - point.y * rotation_speed.sin(),
+        y: point.x * rotation_speed.sin() + point.y * rotation_speed.cos(),
+    }
+}
+
+fn rotate_player(player: &mut Player, dir: f64) -> () {
+    player.facing = rotate_point(&player.facing, dir * player.rotation_speed);
+    player.camera = rotate_point(&player.camera, dir * player.rotation_speed);
+}
+
+fn move_position(world: &Vec<Vec<usize>>, position: Position, delta: PositionDelta) -> Point {
+    if world[(position.x + delta.x) as usize][(position.y + delta.y) as usize] == 0 {
+        position + delta
+    } else {
+        position
+    }
+}
+
+fn darken_color(color: Color) -> Color {
+    Color::RGB(color.r / 2, color.g / 2, color.b / 2)
 }
 
 fn main() {
@@ -101,7 +156,7 @@ fn main() {
 
     let window = video_subsystem
         .window("Example", map_width as u32, map_height as u32)
-        .fullscreen()
+        // .fullscreen()
         .build()
         .unwrap();
 
@@ -114,13 +169,13 @@ fn main() {
     let mut running = true;
 
     let mut timer = sdl_context.timer().unwrap();
-
-    let mut pos_y: f64 = 22.0;
-    let mut pos_x: f64 = 12.0;
-    let mut dir_x: f64 = -1.0;
-    let mut dir_y: f64 = 0.0;
-    let mut plane_x: f64 = 0.0;
-    let mut plane_y: f64 = 0.66;
+    let mut player = Player {
+        facing: Facing { x: -1.0, y: 0.0 },
+        position: Position { x: 12.0, y: 22.0 },
+        camera: Camera { x: 0.0, y: 0.66 },
+        rotation_speed: 0.0,
+        move_speed: 0.0,
+    };
     let mut time = timer.ticks();
     let mut old_time;
 
@@ -128,50 +183,8 @@ fn main() {
         old_time = time;
         time = timer.ticks();
         let frame_time = (time - old_time) as f64 / 1000.0;
-        let rot_speed: f64 = frame_time * 5.0;
-        let move_speed: f64 = frame_time * 3.0;
-
-        let keys: HashSet<Keycode> = event_pump
-            .keyboard_state()
-            .pressed_scancodes()
-            .filter_map(Keycode::from_scancode)
-            .collect();
-
-        if keys.contains(&Keycode::Right) {
-            let old_dir_x = dir_x;
-            dir_x = dir_x * (-rot_speed).cos() - dir_y * (-rot_speed).sin();
-            dir_y = old_dir_x * (-rot_speed).sin() + dir_y * (-rot_speed).cos();
-            let old_plane_x = plane_x;
-            plane_x = plane_x * (-rot_speed).cos() - plane_y * (-rot_speed).sin();
-            plane_y = old_plane_x * (-rot_speed).sin() + plane_y * (-rot_speed).cos();
-        }
-
-        if keys.contains(&Keycode::Left) {
-            let old_dir_x = dir_x;
-            dir_x = dir_x * rot_speed.cos() - dir_y * rot_speed.sin();
-            dir_y = old_dir_x * rot_speed.sin() + dir_y * rot_speed.cos();
-            let old_plane_x = plane_x;
-            plane_x = plane_x * rot_speed.cos() - plane_y * rot_speed.sin();
-            plane_y = old_plane_x * rot_speed.sin() + plane_y * rot_speed.cos();
-        }
-
-        if keys.contains(&Keycode::Up) {
-            if world_map[(pos_x + dir_x * move_speed) as usize][pos_y as usize] == 0 {
-                pos_x += dir_x * move_speed;
-            }
-            if world_map[pos_x as usize][(pos_y + dir_y * move_speed) as usize] == 0 {
-                pos_y += dir_y * move_speed;
-            }
-        }
-
-        if keys.contains(&Keycode::Down) {
-            if world_map[(pos_x - dir_x * move_speed) as usize][(pos_y) as usize] == 0 {
-                pos_x -= dir_x * move_speed;
-            }
-            if world_map[(pos_x) as usize][(pos_y - dir_y * -move_speed) as usize] == 0 {
-                pos_y -= dir_y * move_speed;
-            }
-        }
+        player.rotation_speed = frame_time * 5.0;
+        player.move_speed = frame_time * 3.0;
 
         for event in event_pump.poll_iter() {
             match event {
@@ -187,24 +200,55 @@ fn main() {
                 _ => {}
             }
         }
+
+        let keys: HashSet<Keycode> = event_pump
+            .keyboard_state()
+            .pressed_scancodes()
+            .filter_map(Keycode::from_scancode)
+            .collect();
+
+        if keys.contains(&Keycode::Right) {
+            rotate_player(&mut player, -1.0);
+        }
+
+        if keys.contains(&Keycode::Left) {
+            rotate_player(&mut player, 1.0);
+        }
+
+        if keys.contains(&Keycode::Up) {
+            let delta = PositionDelta {
+                x: player.facing.x * player.move_speed,
+                y: player.facing.y * player.move_speed,
+            };
+            player.position = move_position(&world_map, player.position, delta);
+        }
+
+        if keys.contains(&Keycode::Down) {
+            let delta = PositionDelta {
+                x: -player.facing.x * player.move_speed,
+                y: -player.facing.y * player.move_speed,
+            };
+            player.position = move_position(&world_map, player.position, delta);
+        }
+
         clear_canvas(&mut canvas);
 
         for x in 0..map_width {
-            let camera_x = 2.0 * (x as f64) / (map_width as f64) - 1.0; //x-coordinate in camera space
-            let ray_dir_x = dir_x + plane_x * camera_x;
-            let ray_dir_y = dir_y + plane_y * camera_x;
+            let camera_x = 2.0 * (x as f64) / (map_width as f64) - 1.0; //x-coordinate in player.camera space
+            let ray_player_facing_x = player.facing.x + player.camera.x * camera_x;
+            let ray_player_facing_y = player.facing.y + player.camera.y * camera_x;
 
             //which box of the map we're in
-            let mut map_x: i32 = pos_x as i32;
-            let mut map_y: i32 = pos_y as i32;
+            let mut map_x: i32 = player.position.x as i32;
+            let mut map_y: i32 = player.position.y as i32;
 
             //length of ray from current position to next x or y-side
             let mut side_dist_x: f64;
             let mut side_dist_y: f64;
 
             //length of ray from one x or y-side to next x or y-side
-            let delta_dist_x = (1.0 / ray_dir_x).abs();
-            let delta_dist_y = (1.0 / ray_dir_y).abs();
+            let delta_dist_x = (1.0 / ray_player_facing_x).abs();
+            let delta_dist_y = (1.0 / ray_player_facing_y).abs();
             let mut perp_wall_dist: f64;
 
             //what direction to step in x or y-direction (either +1 or -1)
@@ -214,19 +258,19 @@ fn main() {
             let mut hit = 0; //was there a wall hit?
             let mut side: i32 = 1; //was a NS or a EW wall hit?
 
-            if ray_dir_x < 0.0 {
+            if ray_player_facing_x < 0.0 {
                 step_x = -1;
-                side_dist_x = (pos_x - (map_x as f64)) * delta_dist_x;
+                side_dist_x = (player.position.x - (map_x as f64)) * delta_dist_x;
             } else {
                 step_x = 1;
-                side_dist_x = (map_x as f64 + 1.0 - pos_x) * delta_dist_x;
+                side_dist_x = (map_x as f64 + 1.0 - player.position.x) * delta_dist_x;
             }
-            if ray_dir_y < 0.0 {
+            if ray_player_facing_y < 0.0 {
                 step_y = -1;
-                side_dist_y = (pos_y - map_y as f64) * delta_dist_y;
+                side_dist_y = (player.position.y - map_y as f64) * delta_dist_y;
             } else {
                 step_y = 1;
-                side_dist_y = (map_y as f64 + 1.0 - pos_y) * delta_dist_y;
+                side_dist_y = (map_y as f64 + 1.0 - player.position.y) * delta_dist_y;
             }
 
             while hit == 0 {
@@ -246,9 +290,11 @@ fn main() {
                 }
             }
             if side == 0 {
-                perp_wall_dist = (map_x as f64 - pos_x + (1.0 - step_x as f64) / 2.0) / ray_dir_x;
+                perp_wall_dist = (map_x as f64 - player.position.x + (1.0 - step_x as f64) / 2.0)
+                    / ray_player_facing_x;
             } else {
-                perp_wall_dist = (map_y as f64 - pos_y + (1.0 - step_y as f64) / 2.0) / ray_dir_y;
+                perp_wall_dist = (map_y as f64 - player.position.y + (1.0 - step_y as f64) / 2.0)
+                    / ray_player_facing_y;
             }
             //Calculate height of line to draw on screen
             let line_height = map_height as f64 / perp_wall_dist;
@@ -263,7 +309,7 @@ fn main() {
                 draw_end = map_height as f64 - 1.0;
             }
 
-            let color = match world_map[map_x as usize][map_y as usize] {
+            let mut color = match world_map[map_x as usize][map_y as usize] {
                 1 => Color::RGB(255, 0, 0),
                 2 => Color::RGB(0, 255, 0),
                 3 => Color::RGB(0, 0, 255),
@@ -271,12 +317,16 @@ fn main() {
                 _ => Color::RGB(255, 255, 0),
             };
 
+            if side == 1 {
+                color = darken_color(color)
+            }
+
             canvas.set_draw_color(color);
 
             canvas
                 .draw_line(
-                    Point::new(x, draw_start as i32),
-                    Point::new(x, draw_end as i32),
+                    SdlPoint::new(x, draw_start as i32),
+                    SdlPoint::new(x, draw_end as i32),
                 )
                 .unwrap();
         }
