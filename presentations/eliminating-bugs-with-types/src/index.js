c6b8191caea09d0ce2315000754192627a7b742e
Jimmy Miller <jimmyhmiller@gmail.com>
2018-09-23 16:16:25 -0400

    Changes

---
+++ presentations/eliminating-bugs-with-types/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Presentation from './Presentation';
+
+ReactDOM.render(
+  <Presentation />,
+  document.getElementById('root')
+);
