7b3b39291321c15987666e11c0f3ebbd3ce51659
Jimmy Miller <jimmyhmiller@gmail.com>
2018-10-02 23:25:26 -0400

    changes

--- rust/lisp/src/main.rs
+++ rust/lisp/src/main.rs
@@ -1,51 +1,36 @@
-use std::iter::Peekable;
-use std::io::Read;
-use std::str::Chars;
-use std::fs::File;
-
 #[derive(Debug)]
 enum Token {
     OpenParen,
     CloseParen,
-    Atom(String)
+    Atom(String),
 }
 
-
-fn parse_atom(c : char, expr: &mut Peekable<Chars>) -> Token {
-    let mut s : String = c.to_string();
-    loop {
-        match expr.peek() {
-            Some('(') => break,
-            Some(')') => break,
-            Some(' ') => break,
-            Some(ch) => s.push(*ch),
-            None => break
-        }
-        expr.next();
+fn to_token(token: &str) -> Token {
+    match token {
+        "(" => Token::OpenParen,
+        ")" => Token::CloseParen,
+        str => Token::Atom(str.to_string()),
     }
-    Token::Atom(s)
 }
 
-fn tokenize(expr : &mut Peekable<Chars>) -> Vec<Token> {
-    let mut tokens: Vec<Token> = vec!();
-    loop {
-        match expr.next() {
-            Some('(') => tokens.push(Token::OpenParen),
-            Some(')') => tokens.push(Token::CloseParen),
-            Some(' ') => {},
-            Some(ch) => tokens.push(parse_atom(ch, expr)),
-            None => break,
-        }
-    }
-    tokens
+fn tokenize(text: String) -> Vec<Token> {
+    text.replace("(", " ( ")
+        .replace(")", " ) ")
+        .split_whitespace()
+        .map(to_token)
+        .collect()
 }
+
+#[derive(Debug)]
+enum Expr {
+    SExpr(Vec<Expr>),
+    Atom(String),
+    Int(i64),
+    Float(f64)
+}
+
+
 
 fn main() {
-
-    let mut f = File::open("/Users/jimmy/Desktop/all2.clj").expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    println!("{:?}", tokenize(&mut "(+ 2 2)".chars().peekable()));
+    println!("{:?}", tokenize("(+ 2 2)".to_string()))
 }
