20819bccf2fb9975cea156b8aea774f8b140820f
Jimmy Miller <jimmyhmiller@gmail.com>
2018-11-07 21:08:30 -0500

    Changes

--- critaeus/standup/minimal-api/index.js
+++ critaeus/standup/minimal-api/index.js
@@ -1,29 +1,79 @@
-const { send, json } = require('micro')
-const { router, get, post } = require('microrouter')
-const s3 = new (require('aws-sdk/clients/s3'))();
+const { send, json } = require('micro');
+const { router, get, post } = require('microrouter');
+const streamToPromise = require('stream-to-promise');
+const crypto = require('crypto');
+const MemoryStream = require('memorystream');
+const UrlPattern = require('url-pattern');
+const path = require('path');
+require('dotenv').config()
 
-const bucket = 'a-bucket-that-i-dont-care-about';
+var fs = require('fs');
+var manta = require('manta');
 
-const writeToS3 = (bucket, key, data) => {
-  return s3.putObject({
-    Bucket: bucket,
-    Key: key,
-    Body: JSON.stringify(data),
-  }).promise()
+var client = manta.createClient({
+    sign: manta.privateKeySigner({
+        key: process.env.MANTA_KEY,
+        keyId: process.env.MANTA_KEY_ID,
+        user: process.env.MANTA_USER
+    }),
+    user: process.env.MANTA_USER,
+    url: process.env.MANTA_URL
+});
+console.log('manta ready: %s', client.toString());
+
+
+const createDirectory = (filePath) => {
+  return new Promise((resolve, reject) => {
+    client.mkdirp(`~~/stor/${path.dirname(filePath)}`, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  })
 }
 
-const readFromS3 = (bucket, key) => {
-  return s3.getObject({
-    Bucket: bucket,
-    Key: key,
+const getObject = (filePath) => {
+  return new Promise((resolve, reject) => {
+    client.get(`~~/stor/${filePath}`, function (err, stream) {
+      if (err) {
+        reject(err);
+      } else {
+        stream.setEncoding('utf8');
+        streamToPromise(stream)
+          .then(data => resolve(JSON.parse(data.toString())))
+      }
+    });
   })
-  .promise()
-  .then(data => JSON.parse(data.Body.toString()))
+}
+
+const putObject = (filePath, data) => {
+  const json = JSON.stringify(data);
+  var opts = {
+      md5: crypto.createHash('md5').update(json).digest('base64'),
+      size: Buffer.byteLength(json),
+      type: 'application/json'
+  };
+  var stream = new MemoryStream();
+  return createDirectory(filePath)
+    .then(_ => {
+      return new Promise((resolve, reject) => {
+        client.put(`~~/stor/${filePath}`, stream, opts, function (err) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        });
+        stream.end(json);
+      })
+    })
 }
 
 const handleKeyNotFound = (e, cb) => {
-  if (e.code === "NoSuchKey") {
-    cb(400, {error: 'Status not found'})
+  if (e.code === "ResourceNotFound") {
+    cb(404, {error: 'Status not found'})
   } else {
     console.log(e)
     cb(500, {error: 'Unexpected error'})
@@ -32,8 +82,9 @@ const handleKeyNotFound = (e, cb) => {
 
 const getDatesStatus = async (req, res) => {
   try {
+    console.log("here")
     const { team, user, date } = req.params;
-    const data = await readFromS3(bucket, `${team}/${date}/${user}.json`)
+    const data = await getObject(`standup/${team}/${date}/${user}.json`)
     send(res, 200, data)
   } catch (e) {
     handleKeyNotFound(e, send.bind(null, res))
@@ -44,7 +95,7 @@ const postDatesStatus = async (req, res) => {
   try {
     const body = await json(req)
     const { team, user, date } = req.params;
-    await writeToS3(bucket, `${team}/${date}/${user}.json`, body)
+    await putObject(`standup/${team}/${date}/${user}.json`, body)
     send(res, 201)
   } catch (e) {
     console.log(e)
@@ -52,8 +103,15 @@ const postDatesStatus = async (req, res) => {
   }
 }
 
+const notfound = (req, res) => send(res, 404, {message: 'Not found'})
+
+// default of router includes dots, which is dangerous
+const urlPatternOpts = {
+  segmentValueCharset: "a-zA-Z0-9-"
+}
 
 module.exports = router(
-  get('/standup/:team/:date/:user', getDatesStatus),
-  post('/standup/:team/:date/:user', postDatesStatus),
+  get(new UrlPattern('/standup/:team/:date/:user', urlPatternOpts), getDatesStatus),
+  post(new UrlPattern('/standup/:team/:date/:user', urlPatternOpts), postDatesStatus),
+  get("/*", notfound),
 )
