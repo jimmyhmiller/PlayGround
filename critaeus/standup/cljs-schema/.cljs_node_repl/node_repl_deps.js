7a573ea6bacc4286bc0647c338cd11fc3ca45c7c
Jimmy Miller <jimmyhmiller@gmail.com>
2018-12-07 15:59:24 -0500

    Changes

--- critaeus/standup/cljs-schema/.cljs_node_repl/node_repl_deps.js
+++
@@ -1,2 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.Uri', 'goog.object', 'goog.math.Integer', 'goog.string.StringBuffer', 'goog.array', 'goog.math.Long']);
