99c0eb8afb4d0431d830f7ebc47f45076fe59cf3
Jimmy Miller <jimmyhmiller@gmail.com>
2018-12-10 00:16:48 -0500

    Changes

--- critaeus/standup/cljs-schema/src/cljs-schema/core.cljs
+++ critaeus/standup/cljs-schema/src/cljs-schema/core.cljs
@@ -1,4 +1,237 @@
 (ns cljs-schema.core
-  (:require [faunadb]))
+  (:require [faunadb]
+            [dotenv]
+            [clojure.pprint :as pprint]
+            [clojure.string :as string]))
 
-(def q (.-query faunadb))
+(.config dotenv)
+
+
+
+
+(def q (.. faunadb -query))
+(def Client (.-Client faunadb))
+(def CreateClass (.-CreateClass q))
+(def CreateDatabase (.-CreateDatabase q))
+(def CreateFunction (.-CreateFunction q))
+(def Create (.-Create q))
+(def CreateIndex (.-CreateIndex q))
+(def Index (.-Index q))
+(def Class (.-Class q))
+(def Get (.-Get q))
+(def Match (.-Match q))
+(def Update (.-Update q))
+(def Delete (.-Delete q))
+(def Select (.-Select q))
+(def Append (.-Append q))
+(def Let (.-Let q))
+(def Var (.-Var q))
+(def Date (.-Date q))
+(def Ref (.-Ref q))
+(def Paginate (.-Paginate q))
+(def Map (.-Map q))
+(def Lambda (.-Lambda q))
+(def Casefold (.-Casefold q))
+(def Query (.-Query q))
+(def Call (.-Call q))
+(def Function (.-Function q))
+
+(def client (Client. #js{:secret (.. js/process -env -SECRET)}))
+
+(defn query [client query]
+  (.query client (clj->js query)))
+
+(def debug (atom nil))
+
+(defn print-result [p]
+  (-> p
+      (.then (fn [val] (reset! debug val) val))
+      (.catch (fn [val] (reset! debug val) val))
+      (.then (comp pprint/pprint js->clj))
+      (.catch (comp pprint/pprint js->clj))))
+
+(print-result
+ (query client
+        (Create (Class "teams") {:data {:name "stuff"}})))
+
+(print-result
+ (query client
+        (Create (Class "statuses")
+                (clj->js
+                 {:data {:user (Ref (Class "users") "217993811723289100")
+                         :date (Date (today))
+                         :team (Ref (Class "teams") "217993851172815371")
+                         :status "I did something today"}}))))
+
+
+
+
+(print-result
+ (query client
+        (Update (Ref (Class "statuses") "218148332424397323")
+                {:data {:status "I really did nothing today"}})))
+
+(print-result
+ (query client
+        (CreateIndex
+         (clj->js
+          {:name "statuses-by-team-date"
+           :source (Class "statuses")
+           :terms [{:field ["data" "team"]}
+                   {:field ["data" "date"]}]
+           :values [{:field ["data" "user"]}
+                    {:field ["data" "status"]}]}))))
+
+
+
+(print-result
+ (query client
+        (CreateIndex
+         (clj->js
+          {:name "team-by-name"
+           :source (Class "teams")
+           :terms [{:field ["data" "name"] :transform "casefold"}]
+           :values [{:field ["ref"]}]
+           :unique true}))))
+
+
+(defn team-by-name [name]
+  (Select #js ["ref"]
+          (Get (Match (Index "team-by-name")
+                      (Casefold name)))))
+
+(print-result
+ (query client
+        (CreateIndex
+         (clj->js
+          {:name "user-by-name"
+           :source (Class "users")
+           :terms [{:field ["data" "name"] :transform "casefold"}]
+           :values [{:field ["ref"]}]
+           :unique true}))))
+
+(defn user-by-name [name]
+  (Select #js ["ref"]
+          (Get (Match (Index "user-by-name")
+                      (Casefold name)))))
+
+
+
+(print-result
+ (query client
+        (Let (clj->js {:user (user-by-name "jimmy")
+                      :team (team-by-name "Awesome")})
+             (Create (Class "statuses")
+                     (clj->js
+                      {:data {:user (Var "user")
+                              :date (Date (today))
+                              :team (Var "team")
+                              :status "I made a function today"}})))))
+
+
+(->> (user-by-name "jimmy")
+     (query client)
+     print-result)
+
+(print-result
+ (query client))
+
+(->
+ (query client
+        (CreateIndex
+         (clj->js
+          {:name "status-index"
+           :source (Class "statuses")}))))
+
+(js->clj
+ (.parse js/JSON (.stringify js/JSON @debug)))
+
+(print-result
+ (query client (Get (Ref (Class "teams") "217993811723289100"))))
+
+
+(->>
+ (Get (Function "add-status"))
+ (query client)
+ print-result)
+
+(->>
+ (CreateFunction
+  (clj->js {:name "add-status"
+                   :body
+                   (Query
+                    (Lambda #js ["user-name" "team-name" "date" "status"]
+                            (Let (clj->js {:user (user-by-name (Var "user-name"))
+                                           :team (team-by-name (Var "team-name"))})
+                                 (Create (Class "statuses")
+                                         (clj->js
+                                          {:data {:user (Var "user")
+                                                  :date (Date (Var "date"))
+                                                  :team (Var "team")
+                                                  :status (Var "status")}})))))}))
+ (query client)
+ print-result)
+
+(-> 
+ (query client
+        (CreateFunction
+         (clj->js {:name "get-team-status" 
+                   :body 
+                   (Query (Lambda #js ["date" "team-name"]
+                                  (Let (clj->js {:team (team-by-name (Var "team-name"))})
+                                       (Map
+                                        (Paginate
+                                         (Match
+                                          (Index "statuses-by-team-date")
+                                          (Var "team")
+                                          (Date (Var "date"))))
+                                        (Lambda (clj->js ["user" "status"]) 
+                                                #js [(Select (clj->js ["data", "name"]) (Get (Var "user")))
+                                                     (Var "status")])))))})))
+ print-result)
+
+(->
+ (.query client
+         (Let (clj->js {:team (Select #js ["ref"] (Get (Match (Index "team-by-name") (Casefold "Awesome"))))})
+              (Map
+               (Paginate
+                (Match
+                 (Index "statuses-by-team-date")
+                 (Var "team")
+                 (Date "2018-12-09")))
+               (Lambda (clj->js ["user" "status"]) 
+                       #js [(Select (c-lj->js ["data", "name"]) (Get (Var "user")))
+                            (Var "status")]))))
+ (.then (fn [x] (println x))))
+
+
+(defn get-team-status [date team-name]
+  (->> (Call (Function "get-team-status") 
+             #js [date team-name])
+       (query client)))
+
+(defn add-status [user-name team-name date status]
+  (->> (Call (Function "add-status") 
+             #js [user-name team-name date status])
+       (query client)))
+
+
+(js->clj (.parse js/JSON (.stringify js/JSON @debug)))
+
+(type @debug)
+
+(.keys js/Object (aget (.-data @debug) 0))
+
+(.-tail (aget (.-data @debug) 0))
+
+(first (second (first (js->clj @debug))))
+ 
+(defn today []
+  (-> (js/Date.)
+      .toISOString
+      (string/split #"T")
+      first))
+
+
+;; Get Status by team and date
+;; Make Status
--- critaeus/standup/schema/index.js
+++ critaeus/standup/schema/index.js
@@ -1,37 +1,67 @@
-require('dotenv').config()
-const faunadb = require('faunadb');
+require("dotenv").config();
+const faunadb = require("faunadb");
 const q = faunadb.query;
-
 
 // This is getting really clunky. Should I just try a clojurescript repl?
 
-const log = (...x) => console.log(x);
-const client = new faunadb.Client({ secret: process.env.SECRET })
+const log = (x) => {
+  console.log(x)
+  return x;
+};
+const client = new faunadb.Client({ secret: process.env.SECRET });
 
-const queryLog = (query) => () => {
+const queryLog = query => () => {
   console.log(query);
-  return client.query(query)
+  return client
+    .query(query)
     .then(log)
-    .catch(log)
-}
+    .catch(log);
+};
 
-const runQuery = (query) => queryLog(query)();
+const runQuery = query => queryLog(query)();
 
 const schema = [
-  q.CreateDatabase({ name: "standup"}),
+  q.CreateDatabase({ name: "standup" }),
   q.CreateClass({ name: "users" }),
   q.CreateClass({ name: "teams" }),
-  q.CreateClass({ name: "statuses" }),
-]
+  q.CreateClass({ name: "statuses" })
+];
 
 const jimmy = q.Ref(q.Class("users"), "217993811723289100");
 // runQuery(q.Create(q.Class("users"), {data: {name: "Jimmy"}}))
 // runQuery(q.Update(
-//     q.Ref(q.Class("users"), "217993811723289100"), 
+//     q.Ref(q.Class("users"), "217993811723289100"),
 //     { credentials: { password: "password" } }))
 
+// runQuery(q.Login(jimmy, { password: "password" }))
 
-runQuery(q.Login(jimmy, { password: "password" }))
+
+console.log(JSON.stringify(q.Map(
+    q.Paginate(
+      q.Match(
+        q.Index("statuses-by-team-date"),
+        q.Ref(q.Class("teams"), "217993811723289100"),
+        q.Date("2018-12-08")
+      )
+    ),
+    q.Lambda(["user", "status"], [q.Select(["data", "name"], q.Get(q.Var("user"))), q.Var("status")])
+  )))
+
+runQuery(
+  q.Map(
+    q.Paginate(
+      q.Match(
+        q.Index("statuses-by-team-date"),
+        q.Ref(q.Class("teams"), "217993811723289100"),
+        q.Date("2018-12-08")
+      )
+    ),
+    q.Lambda(["user", "status"], [q.Select(["data", "name"], q.Get(q.Var("user"))), q.Var("status")])
+  )
+).then(data => data.data)
+.catch(log)
+.then(log)
+
 
 
 
@@ -40,13 +70,11 @@ runQuery(q.Login(jimmy, { password: "password" }))
 
 // runQuery(q.Create(q.Class("teams"), {data: {name: "Awesome"}}))
 
-
-const runQueries = (queries) => {
+const runQueries = queries => {
   queries
     .reduce((p, query) => {
-      return p.then(queryLog(query))
+      return p.then(queryLog(query));
     }, Promise.resolve(null))
     .then(log)
-    .catch(log)
-}
-
+    .catch(log);
+};
