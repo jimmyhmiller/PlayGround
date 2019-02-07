569881276bc48aa8029f5b4f83fe1fb55e3b85e6
Jimmy Miller <jimmyhmiller@gmail.com>
2019-02-07 00:57:36 -0500

    Changes

---
+++ codegen/src/codegen/core.cljs
@@ -0,0 +1,85 @@
+(ns codegen.core
+  (:require ["@jimmyhmiller/estel-estree-builder" :as builder]
+            [clojure.walk :as walk]
+            [astring :as codegen]))
+
+
+(defn describe [type]
+  (mapv keyword (keys (dissoc (js->clj ((aget builder (name type)) #js {}))
+                             "type"
+                             "loc"))))
+
+(defn view-node-types []
+  (sort
+   (map (juxt identity describe)
+        (map keyword
+             (js->clj
+              (.keys js/Object builder))))))
+
+(defn process-ast [ast]
+  (walk/postwalk
+   (fn [expr]
+     (cond (and (vector? expr)
+                (not (map-entry? expr))
+                (keyword? (first expr)))
+           (let [node-type (name (first expr))
+                 node-builder (aget builder node-type)]
+             (if-not node-builder
+               (throw (ex-info (str "No node type found " node-type) {:node-type node-type
+                                                                      :expr expr}))
+               (node-builder (second expr))))
+           (map? expr) (clj->js expr)
+           :else expr))
+   ast))
+
+(defn compile [ast]
+  (codegen/generate (process-ast ast)))
+
+(describe :variableDeclaration)
+(view-node-types)
+
+(defn equal-or-var? [x y]
+  (or (symbol? y) (= x y)))
+
+(defn pattern-matches [values pattern]
+  (if (not= (count values) (count pattern))
+    false
+    (every? (partial apply equal-or-var?) (map vector values pattern))))
+
+
+(pattern-matches [1] [1])
+(pattern-matches [2] [2])
+(pattern-matches [2] '[n])
+
+(def example
+  [:functionDeclaration
+   {:id [:identifier {:name "helloWorld"}]
+    :params [[:identifier {:name "name"}]]
+    :body [:functionBody
+           {:body
+            [[:returnStatement
+              {:argument [:binaryExpression
+                          {:operator "+"
+                           :left [:literal {:value "Hello"}]
+                           :right [:identifier {:name "name"}]}]}]]}]}])
+
+(def example2
+  [:program
+   {:body
+    [[:variableDeclaration
+      {:declarations
+       [[:variableDeclarator
+         {:id [:identifier {:name "f"}]
+          :init [:arrowFunctionExpression
+                 {:params [[:identifier {:name "x"}]]
+                  :body [:identifier {:name "x"}]}]}]]
+       :kind "const"}]]}])
+
+
+
+
+
+(compile example)
+
+(compile example2)
+
