2f5c452388d41c0942f3745f0194982150e357a7
Jimmy Miller <jimmyhmiller@gmail.com>
2019-02-12 00:09:48 -0500

    Changes

--- micro-clj/src/micro_clj/server.clj
+++ micro-clj/src/micro_clj/server.clj
@@ -1,8 +1,10 @@
 (ns micro-clj.server)
 
-(defmulti -main (fn [{:keys [uri]}] uri))
+(defmulti -main 
+  (fn [{:keys [uri request-method]}]
+    [request-method uri]))
 
-(defmethod -main "/thing" [req]
+(defmethod -main [:get "/thing"] [req]
   {:thing true})
 
 (defmethod -main :default [req]
