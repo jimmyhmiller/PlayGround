4666f53f14d62b945ef1b2eb286d83f6525a7777
Jimmy Miller <jimmyhmiller@gmail.com>
2019-02-24 15:47:25 -0500

    Changes

--- presentations/code-write-itself/src/presentation.js
+++ presentations/code-write-itself/src/presentation.js
@@ -22,19 +22,68 @@ import createTheme from 'spectacle/lib/themes/default';
 // Require CSS
 require('normalize.css');
 
+const images = {
+  me: require("./images/me.jpg"),
+};
+
+
+preloader(images);
+require("./langs")
 
 export default () =>
   <Presentation>
     <Headline
       textAlign="left"
-      subtextSize={2}
-      subtextCaps={true}
-      text="Don't write tests"
-      subtext="Generate them" />
+      text="Let your code write itself" />
+
+    <TwoColumn
+      title="About Me"
+      left={<Image src={images.me} />}
+      right={
+        <div style={{paddingTop: 80}}>
+          <Text textColor="blue" textSize={60} textAlign="left">Jimmy Miller</Text>
+          <Points noSlide styleContainer={{paddingTop: 10}}>
+            <Point textSize={40} text="Self Taught" /> 
+            <Point textSize={40} text="Senior Developer - Adzerk" /> 
+            <Point textSize={40} text="FP Nerd" />
+          </Points>
+        </div>
+      } 
+    />
+
     <Headline
       textAlign="left"
-      subtextSize={2}
-      subtextCaps={true}
-      text="Don't write tes"
-      subtext="Generate them" />
+      text="Let your code write itself" />
+
+    <Headline
+      textAlign="left"
+      color="blue"
+      text="Not a gimmick" />
+
+    <Points title="Why?">
+      <Point text="Error prone" />
+      <Point text="Waste of time" />
+      <Point text="What programming is about" />
+    </Points>
+
+    <Headline
+      textAlign="left"
+      color="green"
+      text="Programming is thought systemetized" />
+
+    <Headline
+      textAlign="left"
+      color="magenta"
+      text="Languages should let us think, not inhibit us" />
+
+    <Points title="Approach">
+      <Point text="Idris - Static" />
+      <Point text="Barliman - Dynamic" />
+      <Point text="Both experimental" />
+    </Points>
+
+    
+
+    <BlankSlide />
+
   </Presentation>
