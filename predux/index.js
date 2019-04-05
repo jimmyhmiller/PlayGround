793d68da932f92ee7d53c997e33068aaf3fcb2a2
Jimmy Miller <jimmyhmiller@gmail.com>
2019-04-04 21:01:24 -0400

    Changes

--- predux/index.js
+++ predux/index.js
@@ -1,75 +1,31 @@
-require('now-env')
-const multi = require("multiple-methods")
-const produce = require("immer").produce
-const util = require('util')
+const { createTransactor } = require("./transactions");
+const { json, send } = require("micro");
+const multi = require("multiple-methods");
 
-const faunadb = require("faunadb");
-const q = faunadb.query;
+const reducer = multi((_, action) => action.type);
 
-const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET });
+reducer.method("INCREMENT", (state, _) => ({
+  count: state.count + 1
+}));
 
-const log = console.log;
-console.log = x => log(util.inspect(x, false, null, true))
+reducer.method("DECREMENT", (state, _) => ({
+  count: state.count - 1
+}));
 
-const translate = multi(x => x.op)
-translate.method("replace", ({ op, path, value }) => {
-  return x => {
-    if (path && path.length) { 
-      return q.Update(x, {
-          data: {
-            [path]: value
-          }
-        })
-    } else {
-      return q.Replace(x, {data: value})
-    }
-  }
-})
+reducer.method("SET", (state, { count }) => ({
+  count: count
+}));
 
-const transform = (state, draftfn) => {
-  return new Promise(resolve => {
-    produce(state, draftfn, (patches) => console.log(patches) || resolve(patches.map(translate)))
-  })
-}
+reducer.method("GET", (state, _) => state);
 
-const transact = (reducer, initialState) => async (id) => {
-  const state = await client.query(
-    q.Do(
-      q.If(q.Not(q.Exists(q.Match(q.Index("state-by-id"), id))),
-        q.Create(q.Class("state"), {data: {id, ...initialState}}),
-        1),
-    q.Get(q.Match(q.Index("state-by-id"), id))))
-  console.log(state)
-  const queries = await transform(state.data, reducer)
-  await client.query(q.Do(queries.map(x => x(state.ref))))
-}
+reducer.defaultMethod(state => state || { count: 0 })
 
 
-// I've got something seriously powerful here. A persistent reducer.
-// Got to think about the other parts of redux to consider if they make sense here.
-// Immer is a good stepping stone, but it doesn't record things the way I need for 
-// consistency.
+const transact = createTransactor(reducer);
 
-// Could I actually  use timestamp for consistency?
-// If so, I don't have to care how your code is arranged.
+module.exports = async (req, res) => {
+  const { id, action } = await json(req);
+  const result = await transact(id, action);
 
-// Whatever I do, asserting is also a good idea.
-const run = async () => {
-
-  try {
-    await transact(x => ({ ...x, count: x.count + 1 }), {count: 0})("122")
-  } catch (e) {
-    console.error(e)
-  }
-  
-}
-run()
-
-
-
-
-
-
-module.exports = (req, res) => {
-  res.end(`Hello from Node.js on Now 2.0!`);
+  send(res, 200, result);
 };
