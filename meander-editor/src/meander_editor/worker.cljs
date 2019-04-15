dadd971168cd10fdf1bca31cda1d4d0f5fcd19b1
Jimmy Miller <jimmyhmiller@gmail.com>
2019-04-14 22:51:20 -0400

    Changes

--- meander-editor/src/meander_editor/worker.cljs
+++ meander-editor/src/meander_editor/worker.cljs
@@ -35,14 +35,20 @@
                                           [:error result])))))))
 
 (defn eval-meander-many [input lhs rhs]
-    (js/Promise.all
-     (map (fn [coll]
-            (eval-promise (str "(meander/match " coll " " lhs " " rhs ")")))
-          (read-string input))))
-
+  (.then (eval-promise (str "(fn [coll] (try [:success (meander/match coll " lhs " " rhs ")] (catch js/Object e [:error e])))"))
+         (fn [[_ meander-fn]]
+           (let [result (atom nil)
+                 answer (map meander-fn (read-string input))]
+             
+             (reset! result answer)
+            
+             @result))))
 
 ;; This works except for this issue:
 ;; https://github.com/thheller/shadow-cljs/blob/da2fc8f8b10daeee0c984a10f9437a579933931c/src/main/shadow/cljs/bootstrap/browser.cljs#L37-L43
+
+
+;; If I save the input instead of sending it everytime things should be much faster.
 
 (js/self.addEventListener "message"
   (fn [^js e]
