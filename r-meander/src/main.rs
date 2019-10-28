942da48bc99db6a7f7b9ab14fc87f0a15f760537
Jimmy Miller <jimmyhmiller@gmail.com>
2019-10-28 00:58:12 -0400

    Changes

--- r-meander/src/main.rs
+++ r-meander/src/main.rs
@@ -227,6 +227,9 @@ fn rpz(p1 : Pattern) -> Pattern {
     Pattern::RepeatZeroOrMore(Box::new(p1))
 }
 
+fn b<T>(t : T) -> Box<T> {
+    Box::new(t)
+}
 
 
 fn main() {
@@ -252,13 +255,46 @@ fn main() {
     //
     //         PatternResult::Result(env))
     // );
-    let mut states = to_state_machine(Pattern::Concat(Box::new(Pattern::LogicVariable("x".to_string())), Box::new(Pattern::IntegerConstant(2))));
+    // let mut states = to_state_machine(
+    //     Pattern::Concat(
+    //         Box::new(Pattern::Wildcard),
+    //         Box::new(
+    //             Pattern::Concat(
+    //                 Box::new(Pattern::LogicVariable("x".to_string())),
+    //                 Box::new(Pattern::IntegerConstant(2))
+    //             )
+    //         )
+    //     )
+    // );
+
+
+    let mut states = to_state_machine(
+        Pattern::Concat(
+            b(
+                Pattern::RepeatZeroOrMore(
+                    b(Pattern::Concat(b(Pattern::IntegerConstant(1)), b(
+                        Pattern::RepeatZeroOrMore(b(Pattern::LogicVariable("y".to_string())))
+                    )))
+                )
+            ),
+            b(Pattern::Concat(
+                b(
+                    Pattern::RepeatZeroOrMore(b(Pattern::LogicVariable("x".to_string())))
+                ),
+                b(Pattern::IntegerConstant(3))
+            ))
+        )
+    );
+
+
     states.sort_by_key(|s| s.label);
     for state in states {
         println!("{:?}", state)
     }
 
 }
+
+
 
 type Label = i32;
 
@@ -267,12 +303,12 @@ type Label = i32;
 enum Op {
     NOOP,
     Return,
+    Fail,
     SKIP,
     CheckString(String),
     CheckInteger(i32),
     Accumulate(String),
-    Assign(String),
-    CheckVar(String),
+    LogicVariableAssign(String),
 }
 
 #[derive(Debug)]
@@ -299,63 +335,67 @@ fn to_state_machine(pattern : Pattern) -> Vec<State> {
     let mut else_context = VecDeque::new();
     else_context.push_front(END_STATE);
 
-    // How to deal with last node before end?
+    let mut next_context = VecDeque::new();
+
+    next_context.push_front(current_label + 1);
+
 
     loop {
+        println!("{:?}", else_context);
         if let Some(pattern) = queue.pop_front() {
+            let next = next_context.pop_front().unwrap();
+            let else_case = if queue.is_empty() { END_STATE } else { else_context.pop_front().unwrap_or(END_STATE) };
+
             match pattern {
                 Pattern::Wildcard => {
-                    states.push(State { label: current_label, operation: Op::SKIP, then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
+                    states.push(State { label: current_label, operation: Op::SKIP, then_case: next, else_case: else_case });
                 }
                 // Really should go to current fail not end
                 Pattern::IntegerConstant(i) => {
-                    states.push(State { label: current_label, operation: Op::CheckInteger(i), then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
+                    states.push(State { label: current_label, operation: Op::CheckInteger(i), then_case: next, else_case: else_case });
                 }
                 Pattern::StringConstant(s) => {
-                    states.push(State { label: current_label, operation: Op::CheckString(s), then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
+                    states.push(State { label: current_label, operation: Op::CheckString(s), then_case: next, else_case: else_case });
                 }
                 Pattern::MemoryVariable(name) => {
-                    states.push(State { label: current_label, operation: Op::Accumulate(name), then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
+                    states.push(State { label: current_label, operation: Op::Accumulate(name), then_case: next, else_case: else_case });
                 }
                 Pattern::LogicVariable(name) => {
-                    states.push(State { label: current_label, operation: Op::CheckVar(name.clone()), then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
-                    states.push(State { label: current_label, operation: Op::Assign(name), then_case: current_label +1, else_case: *else_context.front().unwrap_or(&END_STATE) });
-                    current_label += 1;
+                    states.push(State { label: current_label, operation: Op::LogicVariableAssign(name), then_case: next, else_case: else_case });
                 }
                 Pattern::RepeatZeroOrMore(pattern) => {
                     queue.push_front(*pattern);
-                    else_context.push_front(current_label);
+                    next_context.push_front(current_label);
+                    if else_case != std::i32::MAX {
+                        else_context.push_front(else_case);
+                    } else {
+                        else_context.push_front(next);
+                    }
 
-                    // I guess in order for this to work I need to change the whole way then works as well.
-                    // Then isn't the current_label + 1. How does this work with a concat?
-                    // I need to think about this part more.
+
+                    continue;
+
                 }
                 Pattern::Concat(p1, p2) => {
-                    queue.push_back(*p1);
-                    queue.push_back(*p2);
+                    queue.push_front(*p2);
+                    queue.push_front(*p1);
+                    next_context.push_back(next);
+                    if else_case != std::i32::MAX {
+                        else_context.push_front(else_case + 1);
+                        else_context.push_front(else_case + 1);
+                    }
+                    continue;
                 }
                 _ => break
             }
+
+            current_label += 1;
+            // Need to figure out how to handle last node
+            next_context.push_back(current_label + 1);
         } else {
             break;
         }
     }
+    println!("{:?}", next_context);
     return states
 }
-
-
-
-
-
-
-
-// State Machine Thoughts
-
-// Operation
-// Then
-// Else
