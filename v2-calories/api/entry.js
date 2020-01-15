0a818862e60a049068003d046130b9e1570f9190
Jimmy Miller <jimmyhmiller@gmail.com>
2020-01-14 21:39:17 -0500

    Changes

--- v2-calories/api/entry.js
+++ v2-calories/api/entry.js
@@ -1,5 +1,7 @@
 const faunadb = require("faunadb");
 const q = faunadb.query;
+const { startOfToday } = require("date-fns"); 
+const {format: formatDate, utcToZonedTime} = require("date-fns-tz");
 
 const client = new faunadb.Client({ secret: process.env.DATABASE_SECRET });
 
@@ -34,6 +36,9 @@ const db = {
   },
 }
 
+
+// Maybe I want to combine these queries? Turn them into functions?
+// Not really sure yet.
 const getTotal = () => {
   return client.query(
     q.Sum(
@@ -53,15 +58,35 @@ const bmr = 2260;
 const getPounds = () => {
   return client.query(
     q.Let({total: q.Sum(
-                q.Select("data", q.Map(
-                  q.Paginate(q.Match("all_entry")),
-                  q.Lambda("x", q.Select(["data", "calories"], q.Get(q.Var("x"))))))),
+                    q.Select("data", q.Map(
+                      q.Paginate(q.Match("all_entry")),
+                      q.Lambda("x", q.Select(["data", "calories"], q.Get(q.Var("x"))))))),
         days: q.Count(q.Distinct(q.Select("data", q.Map(
                   q.Paginate(q.Match("all_entry")),
                   q.Lambda("x", q.Select(["data", "date"], q.Get(q.Var("x"))))))))},
-   q.Divide(q.ToDouble(q.Subtract(q.Multiply(q.Var("days"), 2260), q.Var("total"))), 3500.0))
+   q.Divide(q.ToDouble(q.Subtract(q.Multiply(q.Var("days"), bmr), q.Var("total"))), 3500.0))
   )
 }
+
+const timeZone = 'America/Chicago'
+const today = () => formatDate(utcToZonedTime(startOfToday(), timeZone), "yyyy-MM-dd", { timeZone });
+
+const getRemainingToday = () => {
+  return client.query(
+    // I wish they did the map and filter argument order correctly
+    q.Let({total: q.Sum(
+                      q.Select("data", q.Map(
+                        q.Filter(
+                          q.Paginate(q.Match("all_entry")),
+                          q.Lambda("x", q.Equals(today(), q.Select(["data", "date"], q.Get(q.Var("x")))))),
+                        q.Lambda("x", q.Select(["data", "calories"], q.Get(q.Var("x")))))),
+                      )},
+   // 500 = 1lb per week
+   q.Subtract(bmr, 500, q.Var("total")))
+  )
+}
+
+
 
 
 
@@ -69,8 +94,11 @@ module.exports = async (req, res) => {
   if (req.method === "GET" && req.query.total) {
     res.json({total: await getTotal()});
   }
-  if (req.method === "GET" && req.query.pounds) {
+  else if (req.method === "GET" && req.query.pounds) {
     res.json({total: await getPounds()});
+  }
+  else if (req.method === "GET" && req.query.today) {
+    res.json({remaining: await getRemainingToday()});
   }
   else if (req.method === "GET") {
     res.send(await db.getAll("entry"));
