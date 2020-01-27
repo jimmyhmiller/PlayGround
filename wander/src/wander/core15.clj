939e30ccc0e00fbb827de21ed3d74b4b7c10dd76
Jimmy Miller <jimmyhmiller@gmail.com>
2020-01-27 09:26:21 -0500

    Changes

--- wander/src/wander/core15.clj
+++ wander/src/wander/core15.clj
@@ -7,6 +7,7 @@
       (boolean? m)
       (string? m)
       (symbol? m)
+      (keyword? m)
       (vector? m)
       (map? m)))
 
@@ -59,12 +60,11 @@
                 k)
 
      (if ?pred ?t ?f)
-     (normalize 
-      ?pred 
-      (normalize-name-fn 
-       (fn [t] (k `(if ~t 
-                     ~(normalize ?t)
-                     ~(normalize ?f))))))
+     (normalize-name 
+      ?pred
+      (fn [t] (k `(if ~t 
+                    ~(normalize ?t)
+                    ~(normalize ?f)))))
 
      (?f & ?args)
      (normalize-name 
@@ -106,4 +106,98 @@
 
 
 ;; http://matt.might.net/articles/a-normalization/
+
+
+
+
+(def code '(let
+               [x [1 2 3]]
+             (let
+                 [ret__14025__auto__
+                  (if (vector? x)
+                    (if (= (count x) 3)
+                      (let
+                          [x_nth_0__
+                           (nth x 0)
+                           x_nth_1__
+                           (nth x 1)
+                           x_nth_2__
+                           (nth x 2)]
+                        (let
+                            [?x x_nth_0__]
+                          (let [?y x_nth_1__] (let [?z x_nth_2__] [?x ?y ?z]))))
+                      meander.match.runtime.epsilon/FAIL)
+                    meander.match.runtime.epsilon/FAIL)]
+               (if (meander.match.runtime.epsilon/fail? ret__14025__auto__)
+                 (throw (ex-info "non exhaustive pattern match" '{}))
+                 ret__14025__auto__))))
+
+
+(defn a-normal [code]
+  (m/rewrite code
+
+    (defn ?name ?args ?body)
+    (def ?name (fn ?name ?args (m/cata ?body)))
+
+    (if (m/pred value? ?pred) ?t ?f)
+    (if ?pred
+      (m/cata ?t)
+      (m/cata ?f))
+
+    (m/let [?pred-sym (gensym)] 
+      (if ?pred ?t ?f))
+    (let [?pred-sym (m/cata ?pred)]
+      (if ?pred-sym
+        (m/cata ?t)
+        (m/cata ?f)))
+
+    (let [?x (let [?y ?z] ?body1)] ?body2)
+    (m/cata (let [?y (m/cata ?z)] 
+              (m/cata (let [?x (m/cata ?body1)]
+                        (m/cata ?body2)))))
+
+    (let [thing ?y] ?body)
+    (let [thing ?y] ?body)
+
+    (let [?x ?y] ?body)
+    ;; placeholder need to figure out proper termination, not thing
+    (m/cata (let [thing (m/cata ?y)] (m/cata ?body)))
+    
+    (let [?x ?y . !rest ...] ?body)
+    (m/cata (let [?x ?y]
+              (m/cata (let [!rest ...]
+                        (m/cata ?body)))))
+
+    ((m/pred value? !args) ...)
+    (!args ...)
+
+    (m/let [?sym (gensym)] 
+      ((m/pred value? !values) ..1 . (m/pred (complement value?) ?not) . !rest ...))
+    (m/cata (let [?sym (m/cata ?not)] 
+              (m/cata (!values ... ?sym . !rest ...))))
+
+    (m/let [?sym (gensym)]
+      ((m/pred (complement value?) ?not-value) . !rest ..1))
+    (m/cata (let [?sym (m/cata ?not-value)]
+              (m/cata (?sym . !rest ...))))
+    
+    (m/pred value? ?x) ?x
+
+    ?x (:fall ?x)
+    ))
+
+
+
+(m/rewrite (a-normal code)
+  (let [?var ?val]
+    (m/cata ?body))
+  [[?var ?val] ?body]
+
+  ?x (:fall ?x))
+
+
+(a-normal code)
+
+
+
 
