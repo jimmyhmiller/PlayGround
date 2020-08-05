a522f6939b50aaf1dd9780f5c60c362015ef7aa3
Jimmy Miller <jimmyhmiller@gmail.com>
2020-08-05 18:19:35 -0400

    Changes

--- rust/rules3/src/interpreter/new_main.rs
+++ rust/rules3/src/interpreter/new_main.rs
@@ -33,7 +33,7 @@ pub fn run_new() {
 
     
 
-    read_new("fact(20)", main, &mut program.symbols);
+    read_new("fact(1)", main, &mut program.symbols);
     // if let Some((focus, root)) = program.main.forest.persistent_change(Expr::Symbol(2), n4.unwrap()) {
     //     let result = program.main.forest.garbage_collect(root, focus);
     //     println!("{}: {:?}", root, result);
