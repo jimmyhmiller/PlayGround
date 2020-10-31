ba507adce38cfac9dfed1f3fb3da0ee3a5977b2c
Jimmy Miller <jimmyhmiller@gmail.com>
2020-10-31 01:01:03 -0400

    Changes

---
+++ presentations/kuhn/src/langs.js
@@ -0,0 +1,269 @@
+window.Prism.languages.clojure = {
+    comment: /;+.*/,
+    string: /"(?:\\.|[^\\"\r\n])*"/,
+    operator: /(?:::|[:|'])\b[a-z][\w*+!?-]*\b/i, //used for symbols and keywords
+    keyword: {
+        pattern: /([^\w+*'?-])(?:def|if|do|let|\.\.\.|\.\.|\.\.[0-9]+|=>|quote|var|->>|->|fn|loop|recur|throw|try|monitor-enter|\.|new|set!|def-|defn|defn-|defmacro|defmulti|defmethod|defstruct|defonce|declare|definline|definterface|defprotocol|==|defrecord|>=|deftype|<=|defproject|ns|\*|\+|-|\/|<|=|>|accessor|agent|agent-errors|aget|alength|all-ns|alter|and|append-child|apply|array-map|aset|aset-boolean|aset-byte|aset-char|aset-double|aset-float|aset-int|aset-long|aset-short|assert|assoc|await|await-for|bean|binding|bit-and|bit-not|bit-or|bit-shift-left|bit-shift-right|bit-xor|boolean|branch\?|butlast|byte|cast|char|children|class|clear-agent-errors|comment|commute|comp|comparator|complement|concat|conj|cons|constantly|cond|if-not|construct-proxy|contains\?|count|create-ns|create-struct|cycle|dec|deref|difference|disj|dissoc|distinct|doall|doc|dorun|doseq|dosync|dotimes|doto|double|down|drop|drop-while|edit|end\?|ensure|eval|every\?|false\?|ffirst|file-seq|filter|find|find-doc|find-ns|find-var|first|float|flush|for|fnseq|frest|gensym|get-proxy-class|get|hash-map|hash-set|identical\?|identity|if-let|import|in-ns|inc|index|insert-child|insert-left|insert-right|inspect-table|inspect-tree|instance\?|int|interleave|intersection|into|into-array|iterate|join|key|keys|keyword|keyword\?|last|lazy-cat|lazy-cons|left|lefts|line-seq|list\*|list|load|load-file|locking|long|loop|macroexpand|macroexpand-1|make-array|make-node|map|map-invert|map\?|mapcat|max|max-key|memfn|merge|merge-with|meta|min|min-key|name|namespace|neg\?|new|newline|next|nil\?|node|not|not-any\?|not-every\?|not=|ns-imports|ns-interns|ns-map|ns-name|ns-publics|ns-refers|ns-resolve|ns-unmap|nth|nthrest|or|parse|partial|path|peek|pop|pos\?|pr|pr-str|print|print-str|println|println-str|prn|prn-str|project|proxy|proxy-mappings|quot|rand|rand-int|range|re-find|re-groups|re-matcher|re-matches|re-pattern|re-seq|read|read-line|reduce|ref|ref-set|refer|rem|remove|remove-method|remove-ns|rename|rename-keys|repeat|replace|replicate|resolve|rest|resultset-seq|reverse|rfirst|right|rights|root|rrest|rseq|second|select|select-keys|send|send-off|seq|seq-zip|seq\?|set|short|slurp|some|sort|sort-by|sorted-map|sorted-map-by|sorted-set|special-symbol\?|split-at|split-with|str|string\?|struct|struct-map|subs|subvec|symbol|symbol\?|sync|take|take-nth|take-while|test|time|to-array|to-array-2d|tree-seq|true\?|union|up|update-proxy|val|vals|var-get|var-set|var\?|vector|vector-zip|vector\?|when|when-first|when-let|when-not|with-local-vars|with-meta|with-open|with-out-str|xml-seq|xml-zip|zero\?|zipmap|zipper)(?=[^\w+*'?-])/,
+        lookbehind: true
+    },
+    boolean: /\b(?:true|false|nil)\b/,
+    number: /\b[0-9A-Fa-f]+\b/,
+    punctuation: /[{}[\](),]/
+};
+
+window.Prism.languages.elixir = {
+    comment: {
+        pattern: /#.*/m,
+        lookbehind: true
+    },
+    // ~r"""foo""" (multi-line), ~r'''foo''' (multi-line), ~r/foo/, ~r|foo|, ~r"foo", ~r'foo', ~r(foo), ~r[foo], ~r{foo}, ~r<foo>
+    regex: {
+        pattern: /~[rR](?:("""|''')(?:\\[\s\S]|(?!\1)[^\\])+\1|([/|"'])(?:\\.|(?!\2)[^\\\r\n])+\2|\((?:\\.|[^\\)\r\n])+\)|\[(?:\\.|[^\\\]\r\n])+\]|\{(?:\\.|[^\\}\r\n])+\}|<(?:\\.|[^\\>\r\n])+>)[uismxfr]*/,
+        greedy: true
+    },
+    string: [
+        {
+            // ~s"""foo""" (multi-line), ~s'''foo''' (multi-line), ~s/foo/, ~s|foo|, ~s"foo", ~s'foo', ~s(foo), ~s[foo], ~s{foo} (with interpolation care), ~s<foo>
+            pattern: /~[cCsSwW](?:("""|''')(?:\\[\s\S]|(?!\1)[^\\])+\1|([/|"'])(?:\\.|(?!\2)[^\\\r\n])+\2|\((?:\\.|[^\\)\r\n])+\)|\[(?:\\.|[^\\\]\r\n])+\]|\{(?:\\.|#\{[^}]+\}|[^\\}\r\n])+\}|<(?:\\.|[^\\>\r\n])+>)[csa]?/,
+            greedy: true,
+            inside: {
+                // See interpolation below
+            }
+        },
+        {
+            pattern: /("""|''')[\s\S]*?\1/,
+            greedy: true,
+            inside: {
+                // See interpolation below
+            }
+        },
+        {
+            // Multi-line strings are allowed
+            pattern: /("|')(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+            greedy: true,
+            inside: {
+                // See interpolation below
+            }
+        }
+    ],
+    atom: {
+        // Look-behind prevents bad highlighting of the :: operator
+        pattern: /(^|[^:]):\w+/,
+        lookbehind: true,
+        alias: "symbol"
+    },
+    // Look-ahead prevents bad highlighting of the :: operator
+    "attr-name": /\w+:(?!:)/,
+    capture: {
+        // Look-behind prevents bad highlighting of the && operator
+        pattern: /(^|[^&])&(?:[^&\s\d()][^\s()]*|(?=\())/,
+        lookbehind: true,
+        alias: "function"
+    },
+    argument: {
+        // Look-behind prevents bad highlighting of the && operator
+        pattern: /(^|[^&])&\d+/,
+        lookbehind: true,
+        alias: "variable"
+    },
+    attribute: {
+        pattern: /@\w+/,
+        alias: "variable"
+    },
+    number: /\b(?:0[box][a-f\d_]+|\d[\d_]*)(?:\.[\d_]+)?(?:e[+-]?[\d_]+)?\b/i,
+    keyword: /\b(?:after|alias|and|case|catch|cond|def(?:callback|exception|impl|module|p|protocol|struct)?|do|else|end|fn|for|if|import|not|or|require|rescue|try|unless|use|when)\b/,
+    boolean: /\b(?:true|false|nil)\b/,
+    operator: [
+        /\bin\b|&&?|\|[|>]?|\\\\|::|\.\.\.?|\+\+?|-[->]?|<[-=>]|>=|!==?|\B!|=(?:==?|[>~])?|[*/^]/,
+        {
+            // We don't want to match <<
+            pattern: /([^<])<(?!<)/,
+            lookbehind: true
+        },
+        {
+            // We don't want to match >>
+            pattern: /([^>])>(?!>)/,
+            lookbehind: true
+        }
+    ],
+    punctuation: /<<|>>|[.,%[\]{}()]/
+};
+
+window.Prism.languages.elixir.string.forEach(function(o) {
+    o.inside = {
+        interpolation: {
+            pattern: /#\{[^}]+\}/,
+            inside: {
+                delimiter: {
+                    pattern: /^#\{|\}$/,
+                    alias: "punctuation"
+                },
+                rest: window.Prism.languages.elixir
+            }
+        }
+    };
+});
+
+window.Prism.languages.go = window.Prism.languages.extend("clike", {
+    keyword: /\b(?:break|case|chan|const|continue|default|defer|else|fallthrough|for|func|go(?:to)?|if|import|interface|map|package|range|return|select|struct|switch|type|var)\b/,
+    builtin: /\b(?:bool|byte|complex(?:64|128)|error|float(?:32|64)|rune|string|u?int(?:8|16|32|64)?|uintptr|append|cap|close|complex|copy|delete|imag|len|make|new|panic|print(?:ln)?|real|recover)\b/,
+    boolean: /\b(?:_|iota|nil|true|false)\b/,
+    operator: /[*/%^!=]=?|\+[=+]?|-[=-]?|\|[=|]?|&(?:=|&|\^=?)?|>(?:>=?|=)?|<(?:<=?|=|-)?|:=|\.\.\./,
+    number: /(?:\b0x[a-f\d]+|(?:\b\d+\.?\d*|\B\.\d+)(?:e[-+]?\d+)?)i?/i,
+    string: {
+        pattern: /(["'`])(\\[\s\S]|(?!\1)[^\\])*\1/,
+        greedy: true
+    }
+});
+delete window.Prism.languages.go["class-name"];
+
+window.Prism.languages.haskell = {
+    comment: {
+        pattern: /(^|[^-!#$%*+=?&@|~.:<>^\\/])(--[^-!#$%*+=?&@|~.:<>^\\/].*|{-[\w\W]*?-})/m,
+        lookbehind: !0
+    },
+    char: /'([^\\']|\\([abfnrtv\\"'&]|\^[A-Z@[\]^_]|NUL|SOH|STX|ETX|EOT|ENQ|ACK|BEL|BS|HT|LF|VT|FF|CR|SO|SI|DLE|DC1|DC2|DC3|DC4|NAK|SYN|ETB|CAN|EM|SUB|ESC|FS|GS|RS|US|SP|DEL|\d+|o[0-7]+|x[0-9a-fA-F]+))'/,
+    string: {
+        pattern: /"([^\\"]|\\([abfnrtv\\"'&]|\^[A-Z@[\]^_]|NUL|SOH|STX|ETX|EOT|ENQ|ACK|BEL|BS|HT|LF|VT|FF|CR|SO|SI|DLE|DC1|DC2|DC3|DC4|NAK|SYN|ETB|CAN|EM|SUB|ESC|FS|GS|RS|US|SP|DEL|\d+|o[0-7]+|x[0-9a-fA-F]+)|\\\s+\\)*"/,
+        greedy: !0
+    },
+    keyword: /\b(case|class|data|deriving|do|else|if|in|infixl|infixr|instance|let|module|newtype|of|primitive|then|type|where)\b/,
+    import_statement: {
+        pattern: /(\r?\n|\r|^)\s*import\s+(qualified\s+)?([A-Z][_a-zA-Z0-9']*)(\.[A-Z][_a-zA-Z0-9']*)*(\s+as\s+([A-Z][_a-zA-Z0-9']*)(\.[A-Z][_a-zA-Z0-9']*)*)?(\s+hiding\b)?/m,
+        inside: { keyword: /\b(import|qualified|as|hiding)\b/ }
+    },
+    builtin: /\b(abs|acos|acosh|all|and|any|appendFile|approxRational|asTypeOf|asin|asinh|atan|atan2|atanh|basicIORun|break|catch|ceiling|chr|compare|concat|concatMap|const|cos|cosh|curry|cycle|decodeFloat|denominator|digitToInt|div|divMod|drop|dropWhile|either|elem|encodeFloat|enumFrom|enumFromThen|enumFromThenTo|enumFromTo|error|even|exp|exponent|fail|filter|flip|floatDigits|floatRadix|floatRange|floor|fmap|foldl|foldl1|foldr|foldr1|fromDouble|fromEnum|fromInt|fromInteger|fromIntegral|fromRational|fst|gcd|getChar|getContents|getLine|group|head|id|inRange|index|init|intToDigit|interact|ioError|isAlpha|isAlphaNum|isAscii|isControl|isDenormalized|isDigit|isHexDigit|isIEEE|isInfinite|isLower|isNaN|isNegativeZero|isOctDigit|isPrint|isSpace|isUpper|iterate|last|lcm|length|lex|lexDigits|lexLitChar|lines|log|logBase|lookup|map|mapM|mapM_|max|maxBound|maximum|maybe|min|minBound|minimum|mod|negate|not|notElem|null|numerator|odd|or|ord|otherwise|pack|pi|pred|primExitWith|print|product|properFraction|putChar|putStr|putStrLn|quot|quotRem|range|rangeSize|read|readDec|readFile|readFloat|readHex|readIO|readInt|readList|readLitChar|readLn|readOct|readParen|readSigned|reads|readsPrec|realToFrac|recip|rem|repeat|replicate|return|reverse|round|scaleFloat|scanl|scanl1|scanr|scanr1|seq|sequence|sequence_|show|showChar|showInt|showList|showLitChar|showParen|showSigned|showString|shows|showsPrec|significand|signum|sin|sinh|snd|sort|span|splitAt|sqrt|subtract|succ|sum|tail|take|takeWhile|tan|tanh|threadToIOResult|toEnum|toInt|toInteger|toLower|toRational|toUpper|truncate|uncurry|undefined|unlines|until|unwords|unzip|unzip3|userError|words|writeFile|zip|zip3|zipWith|zipWith3)\b/,
+    number: /\b(\d+(\.\d+)?(e[+-]?\d+)?|0o[0-7]+|0x[0-9a-f]+)\b/i,
+    operator: /\s\.\s|[-!#$%*+=?&@|~.:<>^\\/]*\.[-!#$%*+=?&@|~.:<>^\\/]+|[-!#$%*+=?&@|~.:<>^\\/]+\.[-!#$%*+=?&@|~.:<>^\\/]*|[-!#$%*+=&@|~:<>^\\/]+|`([A-Z][_a-zA-Z0-9']*\.)*[_a-z][_a-zA-Z0-9']*`/,
+    hvariable: /\b([A-Z][_a-zA-Z0-9']*\.)*[_a-z][_a-zA-Z0-9']*\b/,
+    constant: /\b([A-Z][_a-zA-Z0-9']*\.)*[A-Z][_a-zA-Z0-9']*\b/,
+    punctuation: /[{}[\];(),.:]/
+};
+
+(function(prism) {
+    var e = prism.util.clone(prism.languages.javascript);
+    prism.languages.jsx = prism.languages.extend("markup", e);
+    prism.languages.jsx.tag.pattern = /<\/?[\w.:-]+\s*(?:\s+[\w.:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?\s*)*\/?>/i;
+    prism.languages.jsx.tag.inside[
+        "attr-value"
+    ].pattern = /=[^{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
+    var s = prism.util.clone(prism.languages.jsx);
+    delete s.punctuation;
+    s = prism.languages.insertBefore(
+        "jsx",
+        "operator",
+        { punctuation: /=(?={)|[{}[\];(),.:]/ },
+        { jsx: s }
+    );
+    prism.languages.insertBefore(
+        "inside",
+        "attr-value",
+        {
+            script: {
+                pattern: /=(\{(?:\{[^}]*\}|[^}])+\})/i,
+                inside: s,
+                alias: "language-javascript"
+            }
+        },
+        prism.languages.jsx.tag
+    );
+})(window.Prism);
+
+
+
+
+
+
+window.Prism.languages.python = {
+    'comment': {
+        pattern: /(^|[^\\])#.*/,
+        lookbehind: true
+    },
+    'triple-quoted-string': {
+        pattern: /("""|''')[\s\S]+?\1/,
+        greedy: true,
+        alias: 'string'
+    },
+    'string': {
+        pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
+        greedy: true
+    },
+    'function': {
+        pattern: /((?:^|\s)def[ \t]+)[a-zA-Z_]\w*(?=\s*\()/g,
+        lookbehind: true
+    },
+    'class-name': {
+        pattern: /(\bclass\s+)\w+/i,
+        lookbehind: true
+    },
+    'keyword': /\b(?:as|assert|async|await|break|class|continue|def|del|elif|else|except|exec|finally|for|from|global|if|import|in|is|lambda|nonlocal|pass|print|raise|return|try|while|with|yield)\b/,
+    'builtin':/\b(?:__import__|abs|all|any|apply|ascii|basestring|bin|bool|buffer|bytearray|bytes|callable|chr|classmethod|cmp|coerce|compile|complex|delattr|dict|dir|divmod|enumerate|eval|execfile|file|filter|float|format|frozenset|getattr|globals|hasattr|hash|help|hex|id|input|int|intern|isinstance|issubclass|iter|len|list|locals|long|map|max|memoryview|min|next|object|oct|open|ord|pow|property|range|raw_input|reduce|reload|repr|reversed|round|set|setattr|slice|sorted|staticmethod|str|sum|super|tuple|type|unichr|unicode|vars|xrange|zip)\b/,
+    'boolean': /\b(?:True|False|None)\b/,
+    'number': /(?:\b(?=\d)|\B(?=\.))(?:0[bo])?(?:(?:\d|0x[\da-f])[\da-f]*\.?\d*|\.\d+)(?:e[+-]?\d+)?j?\b/i,
+    'operator': /[-+%=]=?|!=|\*\*?=?|\/\/?=?|<[<=>]?|>[=>]?|[&|^~]|\b(?:or|and|not)\b/,
+    'punctuation': /[{}[\];(),.:]/
+};
+
+
+
+window.Prism.languages.rust = {
+    'comment': [
+        {
+            pattern: /(^|[^\\])\/\*[\s\S]*?\*\//,
+            lookbehind: true
+        },
+        {
+            pattern: /(^|[^\\:])\/\/.*/,
+            lookbehind: true
+        }
+    ],
+    'string': [
+        {
+            pattern: /b?r(#*)"(?:\\.|(?!"\1)[^\\\r\n])*"\1/,
+            greedy: true
+        },
+        {
+            pattern: /b?"(?:\\.|[^\\\r\n"])*"/,
+            greedy: true
+        }
+    ],
+    'char': {
+        pattern: /b?'(?:\\(?:x[0-7][\da-fA-F]|u{(?:[\da-fA-F]_*){1,6}|.)|[^\\\r\n\t'])'/,
+        alias: 'string'
+    },
+    'lifetime-annotation': {
+        pattern: /'[^\s>']+/,
+        alias: 'symbol'
+    },
+    'keyword': /\b(?:abstract|alignof|as|be|box|break|const|continue|crate|do|else|enum|extern|false|final|fn|for|if|impl|in|let|loop|match|mod|move|mut|offsetof|once|override|priv|pub|pure|ref|return|sizeof|static|self|struct|super|true|trait|type|typeof|unsafe|unsized|use|virtual|where|while|yield)\b/,
+
+    'attribute': {
+        pattern: /#!?\[.+?\]/,
+        greedy: true,
+        alias: 'attr-name'
+    },
+
+    'function': [
+        /\w+(?=\s*\()/,
+        // Macros can use parens or brackets
+        /\w+!(?=\s*\(|\[)/
+    ],
+    'macro-rules': {
+        pattern: /\w+!/,
+        alias: 'function'
+    },
+
+    // Hex, oct, bin, dec numbers with visual separators and type suffix
+    'number': /\b(?:0x[\dA-Fa-f](?:_?[\dA-Fa-f])*|0o[0-7](?:_?[0-7])*|0b[01](?:_?[01])*|(\d(?:_?\d)*)?\.?\d(?:_?\d)*(?:[Ee][+-]?\d+)?)(?:_?(?:[iu](?:8|16|32|64)?|f32|f64))?\b/,
+
+    // Closure params should not be confused with bitwise OR |
+    'closure-params': {
+        pattern: /\|[^|]*\|(?=\s*[{-])/,
+        inside: {
+            'punctuation': /[|:,]/,
+            'operator': /[&*]/
+        }
+    },
+    'punctuation': /[{}[\];(),:]|\.+|->/,
+    'operator': /[-+*/%!^]=?|=[=>]?|@|&[&=]?|\|[|=]?|<<?=?|>>?=?/
+};
---
+++ presentations/kuhn/src/library.js
@@ -0,0 +1,194 @@
+import React from "react";
+import {
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  ListItem,
+  List,
+  Slide,
+  BlockQuote,
+  Quote,
+} from "spectacle";
+import pre from "spectacle/lib/utils/preloader";
+import createTheme from "spectacle/lib/themes/default";
+
+
+
+export {
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  ListItem,
+  List,
+  Slide,
+  Text,
+} from "spectacle";
+
+
+export const preloader = pre;
+
+// Import theme
+
+
+
+require("normalize.css");
+
+
+
+
+export const theme = createTheme({
+  base03: "#002b36",
+  base02: "#073642",
+  base01: "#586e75",
+  base00: "#657b83",
+  base0: "#839496",
+  base1: "#93a1a1",
+  base2: "#eee8d5",
+  base3: "#fdf6e3",
+  yellow: "#b58900",
+  orange: "#cb4b16",
+  red: "#dc322f",
+  magenta: "#d33682",
+  violet: "#6c71c4",
+  blue: "#268bd2",
+  cyan: "#2aa198",
+  green: "#859900",
+});
+
+
+// Spectacle needs a ref
+class Dark extends React.Component {
+  render() {
+    const { children, ...rest } = this.props;
+    return (
+      <Slide bgColor="base03" {...rest}>
+        {children}
+      </Slide>
+    )
+  }
+}
+
+// Spectacle needs a ref
+export const withSlide = Comp => class WithSlide extends React.Component {
+  render() {
+    const { noSlide=false, slide: Slide = Dark, maxWidth, maxHeight, ...props } = this.props;
+    
+    if (noSlide) {
+      return <Comp {...props} />
+    }
+    return (
+      <Slide {...props} maxWidth={maxWidth || 1300} maxHeight={maxHeight || 800}>
+        <Comp {...props} />
+      </Slide>
+    )
+  }
+}
+
+const removeFirst = (arr) => {
+  arr.shift();
+  return arr
+}
+
+export const detectIndent = source => 
+  source ? / +/.exec(source)[0].length : 0;
+
+export const removeIndent = (indent, source) =>
+  removeFirst(source.split("\n"))
+        .map(s => s.substring(indent, s.length))
+        .join("\n")
+
+
+export const formatCode = (source) => {
+  const spaces = detectIndent(source);
+  return removeIndent(spaces, source)
+}
+
+export const BlankSlide = withSlide(() => {
+  return <span />;
+})
+
+export const Code = withSlide(({ source, color, lang, title, textSize, headlineSize }) => {
+  return (
+    <div>
+      {title ? <Headline color={color} size={headlineSize || 4} noSlide textAlign="left" text={title} /> : null}
+      <CodePane
+        theme="external"
+        textSize={textSize || 36} 
+        source={formatCode(source)}
+        lang={lang} />
+    </div>
+  )
+})
+ 
+export const Point = ({ text, textSize=50 }) => 
+  <ListItem textSize={textSize} textColor="base2">
+    {text}
+  </ListItem>
+
+export const Points = withSlide(({ children, color, title, size=3, styleContainer, caps }) =>
+  <div style={styleContainer}>
+    <Headline noSlide caps={caps} color={color} size={size} textAlign="left" text={title} />
+    <List>
+      {children}
+    </List>
+  </div>
+)
+
+export const QuoteSlide = withSlide(({ text, color }) =>
+  <BlockQuote>
+    <Quote textColor={color}>{text}</Quote>
+  </BlockQuote>
+
+)
+
+export const ImageSlide = ({Slide=Dark, src, height, maxWidth, maxHeight, caps, color, size=3, title, align, ...props }) => 
+  <Slide maxWidth={maxWidth || 1300} maxHeight={maxHeight || 800} {...props}>
+    <Headline noSlide caps={caps} color={color} size={size} textAlign={ align || "left" } text={title} />
+    <Image style={{paddingTop: 30}} src={src} height={height} />
+  </Slide>
+
+export const Subtitle = ({ color="blue", caps, size=5, text, ...props }) =>
+  <Heading caps={caps} textColor={color} size={size} {...props}>
+    {text}
+  </Heading>
+
+export const Headline = withSlide(({ color="magenta", size=2, text, subtext, subtextSize, subtextCaps, textAlign="center", caps=true }) =>
+  <div>
+    <Heading 
+      textAlign={textAlign}
+      size={size} 
+      caps={caps} 
+      lineHeight={1} 
+      textColor={color}
+    >
+      {text}
+    </Heading>
+    {subtext && <Subtitle caps={subtextCaps} text={subtext} textAlign={textAlign} size={subtextSize} />}
+  </div>
+)
+
+export const TwoColumn = withSlide(({ left, right, title, align="left", color="cyan", size=2 }) =>
+  <div style={{paddingTop: 80}}>
+    <Headline color={color} size={size} textAlign={align} noSlide text={title} />
+    { title && <div style={{paddingTop: 80}} />}
+    <Layout>
+      <Fill>
+        {left}
+      </Fill>
+      <Fill>
+        {right}
+      </Fill>
+    </Layout>
+  </div>
+)
+
+export const Presentation = ({ children }) => 
+  <Deck contentWidth={1400} contentHeight={950} theme={theme} controls={false} style={{display: 'none'}} transition={["slide"]} transitionDuration={0} progress="none">
+    {children}
+  </Deck>
