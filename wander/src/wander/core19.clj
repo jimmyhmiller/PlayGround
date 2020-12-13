1f481ff4bf652492bdd0353adb7e99a4a4253b1a
Jimmy Miller <jimmyhmiller@gmail.com>
2020-12-13 01:25:01 -0500

    Changes

--- wander/src/wander/core19.clj
+++ wander/src/wander/core19.clj
@@ -1,24 +1,34 @@
 (ns wander.core19
   (:require [meander.epsilon :as m]))
 
-(def examples 
-  [[:check {:val true :type :boolean :ctx {}}]
-   [:check {:val true :type :int :ctx {}}]
-   [:synth {:val true :ctx {}}]
-   [:synth {:val 1 :ctx {}}]
-   [:synth {:val 'x :ctx {'x :int}}]])
+
+(do
+  (def examples 
+    [[:check {:val true :type :boolean :ctx {}}]
+     [:check {:val true :type :int :ctx {}}]
+     [:check {:val 1 :type :int :ctx {}}]
+     [:check {:val 1 :type :boolean :ctx {}}]
+     [:check {:val 'x :type :boolean :ctx {'x :boolean}}]
+     [:check {:val 'x :type :boolean :ctx {'x :int}}]
+     [:synth {:val true :ctx {}}]
+     [:synth {:val 1 :ctx {}}]
+     [:synth {:val 'x :ctx {'x :int}}]])
 
 
 
-(defn bidirectional [expr]
-  (m/rewrite expr
+  (defn bidirectional [expr]
+    (m/rewrite expr
 
-    [:check {:val (m/or true false) :type :boolean :ctx ?ctx}] ?ctx
+      [:check {:val (m/or true false) :type :boolean :ctx ?ctx}] ?ctx
+      [:check {:val (m/pred int?) :type :int :ctx ?ctx}] ?ctx
+      
+      [:check {:val (m/pred symbol? ?x) :type ?type :ctx {?x ?type :as ?ctx}}] ?ctx
 
-    [:synth {:val (m/or true false)}] :boolean
-    [:synth {:val (m/pred int?)}] :int
+      [:synth {:val (m/or true false)}] :boolean
+      [:synth {:val (m/pred int?)}] :int
 
-    [:synth {:val (m/pred symbol? ?x) :ctx {?x ?t}}] ?t))
+      [:synth {:val (m/pred symbol? ?x) :ctx {?x ?t}}] ?t))
+  
+  (map bidirectional examples))
 
-(map bidirectional examples)
-
+;; need to handle lambda and apply
