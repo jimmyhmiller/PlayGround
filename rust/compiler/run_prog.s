7233369617f9282921a10c04d9a00b8a75c2e37f
Jimmy Miller <jimmyhmiller@gmail.com>
2020-12-23 14:51:51 -0500

    Changes

--- rust/compiler/run_prog.s
+++ rust/compiler/run_prog.s
@@ -1,6 +1,7 @@
 global _main
 
 extern _printf
+extern _malloc
 extern _exit
 section .data
 
@@ -18,35 +19,49 @@ push rbp
 call _exit
 
 main:
+mov rdi, 8000
+call _malloc
+mov r15, rax
 push rbp
 mov rbp, rsp
 sub rsp, 32
 mov qword [rbp-8], 0
 mov rax, 0
-mov qword [rbp-16], 0
+mov qword [rbp-16], 42
+mov rdi, qword [rbp-16]
+mov qword [r15], rdi
+mov qword [rbp-24], 0
 
 loop:
-mov qword [rbp-24], 21
-mov rdi, qword [rbp-16]
-cmp qword [rbp-24], rdi
-mov qword [rbp-24], rcx
-mov rcx, qword [rbp-24]
+mov qword [rbp-32], 21
+mov rdi, qword [rbp-24]
+cmp qword [rbp-32], rdi
+mov qword [rbp-32], rcx
+mov rcx, qword [rbp-32]
 je done
 lea rdi, [format]
-mov rsi, qword [rbp-16]
+mov rsi, qword [rbp-24]
 push rax
 mov rax, 0
 call _printf
 pop rax
-mov qword [rbp-24], 1
+mov qword [rbp-32], 1
 mov rax, 0
-add rax, qword [rbp-16]
 add rax, qword [rbp-24]
-mov qword [rbp-16], rax
+add rax, qword [rbp-32]
+mov qword [rbp-24], rax
 jmp loop
 
 done:
-mov rax, qword [rbp-16]
+mov rdi, qword [r15]
+mov qword [rbp-32], rdi
+lea rdi, [format]
+mov rsi, qword [rbp-32]
+push rax
+mov rax, 0
+call _printf
+pop rax
+mov rax, qword [rbp-32]
 mov rsp, rbp
 pop rbp
 ret
