e5772899458d794a5b600730a7516e0bcf628c26
Jimmy Miller <jimmyhmiller@gmail.com>
2020-12-23 13:50:16 -0500

    Changes

--- rust/compiler/run_prog.s
+++ rust/compiler/run_prog.s
@@ -6,7 +6,7 @@ section .data
 
 format:
 default rel
-db 72, 101, 108, 108, 111, 32, 37, 100, 10, 0
+db 37, 100, 10, 0
 section .text
 
 _main:
@@ -23,15 +23,21 @@ mov rbp, rsp
 sub rsp, 32
 mov qword [rbp-8], 0
 mov rax, 0
-mov qword [rbp-16], 3
+mov qword [rbp-16], 0
 
 loop:
-mov qword [rbp-24], 20
+mov qword [rbp-24], 21
 mov rdi, qword [rbp-16]
 cmp qword [rbp-24], rdi
 mov qword [rbp-24], rcx
 mov rcx, qword [rbp-24]
 je done
+lea rdi, [format]
+mov rsi, qword [rbp-16]
+push rax
+mov rax, 0
+call _printf
+pop rax
 mov qword [rbp-24], 1
 mov rax, 0
 add rax, qword [rbp-16]
@@ -41,12 +47,6 @@ jmp loop
 
 done:
 mov rax, qword [rbp-16]
-lea rdi, [format]
-mov rsi, rax
-push rax
-mov rax, 0
-call _printf
-pop rax
 mov rsp, rbp
 pop rbp
 ret
