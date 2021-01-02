0a9d355d9b6f8c912c4732641d6957891a4e7707
Jimmy Miller <jimmyhmiller@gmail.com>
2021-01-02 08:42:31 -0500

    Changes

--- live-view/client/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { live_view.core.init(); } catch (e) { console.error("An error occurred when calling (live-view.core/init)"); throw(e); }
--- live-view/server/src/live-view-server/core.clj
+++ live-view/server/src/live-view-server/core.clj
@@ -36,7 +36,14 @@
     (transit/read reader)))
 
 
-(defn update-view-and-send-patch [view state internal-state-atom]
+(defn broadcast [clients patch]
+  (doseq [[ws _] clients]
+    ;; Super important to pass this empty map
+    ;; it is the callback handlers. Otherwise
+    ;; we are calling send synchronously.
+    (jetty/send! ws patch)))
+
+(defn update-view-and-send-patch [view state internal-state-atom broadcast]
   (let [new-view-state (view state)
         patch (edit/get-edits (editscript/diff (:view-state @internal-state-atom) new-view-state))
         _ (swap! internal-state-atom assoc :view-state new-view-state)
@@ -44,11 +51,7 @@
         writer (transit/writer out :json)]
     (transit/write writer {:type :patch
                            :value patch})
-    (doseq [[ws _] (:clients @internal-state-atom)]
-      ;; Super important to pass this empty map
-      ;; it is the callback handlers. Otherwise
-      ;; we are calling send synchronously.
-      (jetty/send! ws (.toString out) {}))))
+    (broadcast (:clients @internal-state-atom) (.toString out))))
 
 
 
@@ -76,17 +79,18 @@
 
 ;; TODO: Need to break things apart so you can run this on your own server
 ;; TODO: Need to have options like port
-(defn start-live-view-server [state view event-handler]
+;; TODO: Need to make broadcast overridable, but it needs more context
+(defn start-live-view-server [{:keys [state view event-handler]}]
   (let [internal-state (atom {:clients {}
                               :view-state nil})]
     (swap! internal-state assoc :view-state (view @state))
     (when (var? view)
       (add-watch view :view-updated
                   (fn [_ _ _ new-view-fn]
-                    (update-view-and-send-patch new-view-fn @state internal-state))))
+                    (update-view-and-send-patch new-view-fn @state internal-state #'broadcast))))
     (add-watch state :send-websocket
                (fn [_ _ _ state]
-                 (update-view-and-send-patch view state internal-state)))
+                 (update-view-and-send-patch view state internal-state broadcast)))
 
 
     ;; Is there a performance penalty for the way I am doing the dynamic things here?
@@ -107,35 +111,9 @@
                   :actions []}))
 
 
-(comment
-
-  (swap! state assoc :name "Jimmy!")
-  (swap! state assoc :items [])
-
-  (swap! state assoc :actions [])
-
-
-  (def process
-    (future
-      (let [items (map (fn [x]
-                         (str "item" x))
-                       (range 0 5000))]
-        (loop [offset 0]
-          (if (>= offset 5000)
-            (recur 0)
-            (do
-              (Thread/sleep 16)
-              (swap! state assoc :items (take 50 (drop offset items)))
-              (recur (inc offset))))))))
-
-
-
-  (future-cancel process)
-)
-
 ;; Updating view can cause some state issues
 ;; TODO: FIX
-(defn view [{:keys [name input-value items actions :live]}]
+(defn view [{:keys [name input-value items actions]}]
   [:body
    [:h1 {:style {:color "green"}} "Hello " name]
    [:form {:onsubmit [:submit {}]}
@@ -163,7 +141,62 @@
       (prn action))))
 
 
-(def server (start-live-view-server state #'view #'handle-event))
+(def server (start-live-view-server {:state state 
+                                     :view #'view 
+                                     :event-handler #'handle-event}))
 
 (comment
   (.stop server))
+
+
+
+
+
+;; Need client local state
+;; That means I need to run the view function for each client.
+;; But I should really only do this if I need to.
+;; How can I signal that this is the case?
+
+
+;; How about this?
+;; First we store the view state on each connection.
+;; Because of persistent data structures, if we are smart this shouldn't be too bad.
+;; I would love to just do that and a memoized view function (fifo). But that isn't enough.
+;; If we have any user information, that means the data will be different. :(
+;; So even if we render the same view every time, the input would be different for each user.
+;; So we need to memoize the function, but also have a system for declaring what data we depend on.
+;; Have to think about how the defaults should work here. But should probably follow the principle of maximal
+;; flexibility at the base level and building up a less flexible but more convenient higher-level interface.
+
+
+;; Maybe something like?
+;; :view-depends-on [:state :user-state]
+
+
+
+
+(comment
+
+  (swap! state assoc :name "Jimmy!")
+  (swap! state assoc :items [])
+
+  (swap! state assoc :actions [])
+
+
+  (def process
+    (future
+      (let [items (map (fn [x]
+                         (str "item" x))
+                       (range 0 5000))]
+        (loop [offset 0]
+          (if (>= offset 5000)
+            (recur 0)
+            (do
+              (Thread/sleep 16)
+              (swap! state assoc :items (take 50 (drop offset items)))
+              (recur (inc offset))))))))
+
+
+
+  (future-cancel process)
+)
