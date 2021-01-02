faa94d4071a03f107708a4bb737f1cff1107a5e6
Jimmy Miller <jimmyhmiller@gmail.com>
2021-01-02 08:42:31 -0500

    Changes

--- live-view/client/public/js/cljs-runtime/live_view.test<>.js
+++
@@ -1,6 +0,0 @@
-goog.provide('live_view.test_LT__GT_');
-live_view.test_LT__GT_.greeting = (function live_view$test_LT__GT_$greeting(){
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello world!!"], 0));
-});
-
-//# sourceMappingURL=live_view.test<>.js.map
