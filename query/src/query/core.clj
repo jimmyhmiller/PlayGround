d8f93b67d6f3f23ddd66003b91ec4c31ca1c44d7
Jimmy Miller <jimmyhmiller@gmail.com>
2021-08-11 01:12:22 -0400

    Changes

--- query/src/query/core.clj
+++ query/src/query/core.clj
@@ -4,87 +4,78 @@
 
 
 
-(defn gallop [coll val]
-  (if (and (not (empty? coll))
-           (< (first coll) val))
-    (let [step 1
-          result
-          (loop [coll coll val val step step]
-            (if (and (< step (count coll))
-                     (< (nth coll step) val))
-              (recur (subvec coll step)
-                     val
-                     (bit-shift-left step 1))
-              ;; Would love to get rid of this allocation
-              ;; I could consider deftype with a mutable variable for this stuff
-              [coll step]))
-          coll (nth result 0)
-          step (nth result 1)
-          step (bit-shift-right step 1)
-          coll (loop [coll coll
-                      val val
-                      step step]
-                 (if (> step 0)
-                   (if (and (< step (count coll))
-                            (< (nth coll step) val))
-                     (recur (subvec coll step)
-                            val
-                            (bit-shift-right step 1))
-                     (recur coll val (bit-shift-right step 1)))
-                   coll))]
-      (subvec coll 1)))
-  coll)
+(defn gallop [coll low high compare val ]
+  (let [compare (complement compare)]
+    (if (and (< low high)
+             (compare (nth coll low) val))
+      (let [step 1
+            result (loop [i low val val step step]
+                     (if (and (< (+ i step) high)
+                              (compare (nth coll (+ i step)) val))
+                       (recur (+ i step)
+                              val
+                              (bit-shift-left step 1))
+                       [i step]))
+            i (nth result 0)
+            step (nth result 1)
+            step (bit-shift-right step 1)
+            i (loop [i i
+                     val val
+                     step step]
+                
+                (if (> step 0)
+                  (if (and (< (+ i step) high)
+                           (compare (nth coll (+ i step)) val))
+                    (recur (+ i step)
+                           val
+                           (bit-shift-right step 1))
+                    (recur i val (bit-shift-right step 1)))
+                  i))]
+        (inc i))
+     low)))
 
+;; The other example is lazy, should this be lazy?
 
-
-(gallop [9 10] 1)
-
-
+;; I think I also need this to work on tuples. But I can just change > and >= to work with some tuples.
+;; Then I should get back the tuple with the right value. I think that is how this works, still a bit confused.
+;; Actualy, they are tuples of columns
+;; So how does all of this work?
+;; Maybe instead of returning the value-a I found I need to be returning the value-a of the other index?
+;; I just really don't understand how to put these pieces together.
+;; Or maybe this does make sense?
+;; See below for an example that is seeming right
 (defn intersect [column-a column-b]
   (loop [column-a column-a
          column-b column-b
-         results []]
-    (if (empty? column-a)
-      results
-      (let [value-a (first column-a)
-            column-b (gallop column-b value-a)
-            value-b (first column-b)]
-        (if (and value-a (= value-a value-b))
-          ;; So in the example, they gallop with a > predicate
-          ;; But why? Maybe they allow duplicates?
-          ;; I mean that would make sense in that case.
-          ;; If I allow duplicates I should probably do that.
-          (recur (subvec column-a 1)
-                 (subvec column-b 1)
-                 (conj results value-a))
+         index-a 0
+         index-b 0
+         result []
+         gas 0]
+    
+    (if (or (>= index-a (count column-a))
+           #_ (>= gas 100))
+      result
+      (let [value-a (nth column-a index-a)
+            index-b (gallop column-b index-b (count column-b) >= value-a)
+            value-b (if (>= index-b (count column-b) )
+                      ::not-matching
+                      (nth column-b index-b))]
+        (if (= value-a value-b)
+          (recur column-a
+                 column-b
+                 (gallop column-a index-a (count column-a) > value-a)
+                 (gallop column-b index-b (count column-b) > value-b)
+                 (conj result value-a)
+                 (inc gas))
           (recur column-b
                  column-a
-                 results))))))
+                 index-b
+                 index-a
+                 result
+                 (inc gas)))))))
 
 
 
-
-
-
-
-
-(- (count [3 4 5]) (count [2 3]) )
-(ffirst [[1 2 3] [3 4 5]])
-
-(intersect* [[1 2 3] [3 4 5]]
-            [[1 2 3] [3 4 5]])
-
-
-
-(subvec [3 4 5] 2)
-
-
-
-
-;; Okay, so I think I got some stuff a bit wrong.
-;; It seems that indexes might be tuples of arrays?
-;; So that means I need to gallop to find the values
-;; and I would really need to deal with indexes instead of subvecs?
 
 
 [0 :playlist/name "Heavy Metal Classic"]
@@ -99,21 +90,35 @@
 ;; We can deal with that fact later.
 
 (def indexes
-  {:playlist/name {"Heavy Metal Classic" [0]}
-   :playlist-track/playlist [[0] [1]]
-   :playlist-track/track {1 [2]}
+  {:playlist/name {"Heavy Metal Classic" [0 3]}
+   :playlist-track/playlist {0 [0 1 2 3]
+                             1 [0 1]
+                             3 [2]}
+   :playlist-track/track {0 [2]
+                          1 [3]
+                          2 [5]
+                          3 [1]}
    :track/album {2 [3]}
    :album/artist {3 [4]}
    :artist/name {4 ["Death"]}})
 
 
 
-(intersect (get indexes))
+;; Okay I think this is something like this.
+;; Need to make some real proper indexes (maybe eav, etc) and try it out.
+(time
+ (for [playlist-id (intersect (get-in indexes [:playlist/name "Heavy Metal Classic"])
+                              (into [] (keys (get-in indexes [:playlist-track/playlist]))))]
+   (for [track-id (intersect (get-in indexes [:playlist-track/playlist playlist-id])
+                             (into [] (keys (get-in indexes [:playlist-track/track]))))]
+     [playlist-id track-id (get-in indexes [:playlist-track/track track-id])])))
 
 
 
 
-(let [upper-1 (rand-int 100000)
+
+
+(let [upper-1 (rand-int 1000000)
       lower-1 (rand-int upper-1)
       upper-2 (+ lower-1 (rand-int upper-1))
       lower-2 (rand-int upper-2)
@@ -123,7 +128,6 @@
       column-b (into [] (range lower-2 upper-2))
       column-c (into [] (range lower-3 upper-3))]
   (println (+ (count column-a) (count column-b) (count column-c)))
-  (println lower-1 upper-1 lower-2 upper-2 lower-3 upper-3)
   (time
    (do
      (println
@@ -131,3 +135,7 @@
        (intersect column-c
                   (intersect column-a column-b))))
      nil)))
+
+
+
+
