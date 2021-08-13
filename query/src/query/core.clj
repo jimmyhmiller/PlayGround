769b2f3cfe68cd96fff03d17b0b1eec4958646cc
Jimmy Miller <jimmyhmiller@gmail.com>
2021-08-12 20:27:07 -0400

    Changes

--- query/src/query/core.clj
+++ query/src/query/core.clj
@@ -78,18 +78,143 @@
 
 
 
+
+
+(def query '[[?playlist :playlist/name "Heavy Metal Classic"]
+             [?playlist :playlist/track ?track]
+             [?track :track/name ?track-name]
+             [?track :track/album ?album]
+             [?album :album/name ?album-name]
+             [?album :album/artist ?artist]
+             [?artist :artist/name ?artist-name]])
+
+
 [0 :playlist/name "Heavy Metal Classic"]
 [1 :playlist-track/playlist 0]
 [1 :playlist-track/track 2]
 [2 :track/album 3]
 [3 :album/artist 4]
 [4 :artist/name "Death"]
+[]
+
+
+(def indexes 
+  {:eav {0 {:playlist/name ["Heavy Metal Classic"]
+            :playlist/track [1 2 3]}
+         1 {:track/name ["Track 1"]
+            :track/album [4]}
+         2 {:track/name ["Track 2"]
+            :track/album [4]}
+         3 {:track/name ["Track 3"]
+            :track/album [5]}
+         4 {:album/name ["Debut"]
+            :album/artist [6]}
+         5 {:album/name ["Debut 2"]
+            :album/artist [6]}
+         6 {:artist/name ["Great Band Name"]}}
+   :aev {:playlist/name {0 ["Heavy Metal Classic"]}
+         :track/name {1 ["Track 1"]
+                      2 ["Track 2"]
+                      3 ["Track 3"]}
+         :track/album {1 [4]
+                       2 [4]
+                       3 [5]}
+         :album/name {4 ["Debut"]
+                      5 ["Debut 2"]}
+         :album/artist {4 [6]
+                        5 [6]}
+         :artist/name {6 ["Great Band Name"]}}
+   :vae {"Heavy Metal Classic" {:playlist/name [0]}
+         "Track 1" {:track/name [1]}
+         "Track 2" {:track/name [2]}
+         "Track 3" {:track/name [3]}
+         4 {:track/album [1 2]}  
+         5 {:track/album [3]}
+         "Debut" {:album/name [4]}
+         "Debut 2" {:album/name [5]}
+         6 {:album/artist [4 5]}
+         "Great Band Name" {:artist/name [6]}}})
+
+
+(defn logic-var? [x]
+  (and (symbol? x)
+       (clojure.string/starts-with? (name x) "?")))
+
+
+(defn constant? [x]
+  (not (logic-var? x)))
+
+(defn query-frequencies [query]
+  (->> query
+       (mapcat identity)
+       (filter logic-var?)
+       frequencies))
+
+;; Very wrong function for bound, but going with it till I know something better
+(defn l-bound? [query x]
+  (let [freq (query-frequencies query)]
+    (or (not (logic-var? x))
+        (> (get freq x 0) 1))))
+
+
+
+;; Needs to be better
+
+(defn triple-to-index [query [e a v]]
+  (cond
+    (and (constant? v) (constant? a))
+    [e :vae v a]
+    (and (constant? e) (constant? a))
+    [v :eav e a]
+    (and (l-bound? query e) (l-bound? query a))
+    [v :eav e a]
+    (and (l-bound? query v) (l-bound? query a))
+    [e :vae v a]
+    :else (throw (ex-info "not covered" {:index [e a v]}))))
+
+
+(map (partial triple-to-index query) query)
+
+;; =>
+
+([?playlist :vae "Heavy Metal Classic" :playlist/name]
+ [?track :eav ?playlist :playlist/track]
+ [?track-name :eav ?track :track/name]
+ [?album :eav ?track :track/album]
+ [?album-name :eav ?album :album/name]
+ [?artist :eav ?album :album/artist]
+ [?artist-name :eav ?artist :artist/name])
+
+
+;; =>
+
+;; It looks like with this query I never have to intersect...
+;; Umm, when do I have to intersect?
+
+(time
+ (doall
+  (for [?playlist (get-in indexes [:vae "Heavy Metal Classic" :playlist/name])
+        ?track (get-in indexes [:eav ?playlist :playlist/track])
+        ?track-name (get-in indexes [:eav ?track :track/name])
+        ?album (get-in indexes [:eav ?track :track/album])
+        ?album-name (get-in indexes [:eav ?album :album/name])
+        ?artist (get-in indexes [:eav ?album :album/artist])
+        ?artist-name (get-in indexes [:eav ?artist :artist/name])]
+    {:track/name ?track-name
+     :album/name ?album-name
+     :artist/name ?artist-name})))
+;; 1.7 ms :(
+;; That is soooo slow :(
+
+
+
 
 
 ;; This is mixing up different kinds of indexes
 ;; We can deal with that fact later.
 
-(def indexes
+
+(def old-indexes
   {:playlist/name {"Heavy Metal Classic" [0 3]}
    :playlist-track/playlist {0 [0 1 2 3]
                              1 [0 1]
@@ -104,14 +229,21 @@
 
 
 
+
+[
+ ]
+
+
+
+
 ;; Okay I think this is something like this.
 ;; Need to make some real proper indexes (maybe eav, etc) and try it out.
 (time
- (for [playlist-id (intersect (get-in indexes [:playlist/name "Heavy Metal Classic"])
-                              (into [] (keys (get-in indexes [:playlist-track/playlist]))))]
-   (for [track-id (intersect (get-in indexes [:playlist-track/playlist playlist-id])
+ (for [playlist-id (intersect (get-in old-indexes [:playlist/name "Heavy Metal Classic"])
+                              (into [] (keys (get-in old-indexes [:playlist-track/playlist]))))]
+   (for [track-id (intersect (get-in old-indexes [:playlist-track/playlist playlist-id])
                              (into [] (keys (get-in indexes [:playlist-track/track]))))]
-     [playlist-id track-id (get-in indexes [:playlist-track/track track-id])])))
+     [playlist-id track-id (get-in old-indexes [:playlist-track/track track-id])])))
 
 
 
