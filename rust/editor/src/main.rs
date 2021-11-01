a40db4b1d467372cff9a02753e72531be654e926
Jimmy Miller <jimmyhmiller@gmail.com>
2021-11-01 17:39:10 -0400

    Changes

--- rust/editor/src/main.rs
+++ rust/editor/src/main.rs
@@ -1,10 +1,9 @@
 use std::{cmp::{max, min}, convert::TryInto, fs, ops::Neg};
 
-use sdl2::{*, event::*, keyboard::*, pixels::Color, rect::Rect, render::*, video::*};
+use sdl2::{*, event::*, keyboard::*, libc::fpos_t, pixels::Color, rect::Rect, render::*, video::*};
 
 
 fn render_char(width: i32, height: i32, c: char) -> Rect {
-    let width = width / 94;
     let rect = Rect::new(width * (c as i32 - 33), 0, width as u32, height as u32);
     rect
 }
@@ -19,8 +18,21 @@ fn digit_count(x: usize) -> usize {
     count
 }
 
+
+// This clearly is telling me I'm missing an abstraction
+fn draw_string<'a>(canvas: & mut Canvas<Window>, target: &'a mut Rect, texture: &Texture, text: &str) -> &'a mut Rect {
+    for char in text.chars() {
+        let char_rect : Rect = render_char(target.width() as i32, target.height() as i32, char as char);
+        target.set_x(target.x() + target.width() as i32);
+        canvas.copy(texture, Some(char_rect), Some(*target)).unwrap();
+    }
+    return target;
+}
+
 // It would be pretty cool to add a minimap
 // Also cool to just add my own scrollbar.
+
+// Need to figure out colors for the texture.
 
 fn main() -> Result<(), String> {
 
@@ -42,7 +54,7 @@ fn main() -> Result<(), String> {
 
     
 
-    let window_width: i32 = 500;
+    let mut window_width: i32 = 500;
     let mut window_height: i32 = 500;
 
     let window = sdl_context
@@ -52,8 +64,13 @@ fn main() -> Result<(), String> {
         .build()
         .unwrap();
 
+
     // Let's create a Canvas which we will use to draw in our Window
-    let mut canvas: Canvas<Window> = window.into_canvas().present_vsync().build().unwrap();
+    let mut canvas: Canvas<Window> = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .unwrap();
     let mut event_pump = sdl_context.event_pump()?;
 
     let texture_creator = canvas.texture_creator();
@@ -72,7 +89,7 @@ fn main() -> Result<(), String> {
         .render(text.as_str())
         .blended(Color::RGBA(0, 0, 0, 255))
         .map_err(|e| e.to_string())?;
-    let texture = texture_creator
+    let mut texture = texture_creator
         .create_texture_from_surface(&surface)
         .map_err(|e| e.to_string())?;
 
@@ -81,13 +98,12 @@ fn main() -> Result<(), String> {
     let letter_height = height;
 
     let start_time = std::time::Instant::now();
-    let text = fs::read_to_string("/Users/jimmyhmiller/Desktop/test/test.txt").unwrap();
+    let text = fs::read_to_string("/Users/jimmyhmiller/Desktop/test/test3.txt").unwrap();
     println!("read file in {} ms", start_time.elapsed().as_millis());
     let chars = text.as_bytes();
 
     let mut line_range = Vec::<(usize,usize)>::with_capacity(chars.len()/60);
     let mut line_start = 0;
-    let mut line_end = 0;
     // This is slow. I don't actually have to do this for the whole file in one go.
     // I can do it for the first screen and then start doing more over time.
     // Need that concept in this app.
@@ -105,7 +121,12 @@ fn main() -> Result<(), String> {
     let mut offset_y = 0;
     let mut at_end = false;
     let mut scroll_speed : i32 = 5;
+    let mut frame_counter = 0;
+    let mut time_start = std::time::Instant::now();
+    let mut fps = 0;
+    
     loop {
+
         let mut scroll_y : i32 = 0;
         match event_pump.poll_event() {
             Some(Event::Quit { .. }) => ::std::process::exit(0),
@@ -120,8 +141,8 @@ fn main() -> Result<(), String> {
             }
             // Continuous resize in sdl2 is a bit weird
             // Would need to watch events or something
-            Some(Event::Window {win_event: WindowEvent::Resized(_w, h), ..}) => {
-                // window_width = w;
+            Some(Event::Window {win_event: WindowEvent::Resized(w, h), ..}) => {
+                window_width = w;
                 window_height = h;
             }
 
@@ -135,8 +156,6 @@ fn main() -> Result<(), String> {
             }
             _ => {}
         }
-
-        println!("zoomies! {}", scroll_speed);
 
         if !at_end || scroll_y < 0 {
             offset_y += scroll_y;
@@ -175,22 +194,29 @@ fn main() -> Result<(), String> {
             // Really I should draw a line or something to make it look nicer.
             line_offset += ((digit_count(line_range.len()) - digit_count(i + 1)) * 10) as i32;
             let line_number = (i + 1).to_string();
-            for char in line_number.chars() {
-                let char_rect = render_char(width as i32, height as i32, char as char);
-                target.set_x(line_offset);
-                canvas.copy(&texture, Some(char_rect), Some(target)).unwrap();
-                line_offset += letter_width as i32;
-            }
+            target.set_x(line_offset);
 
+            let target = draw_string(&mut canvas, &mut target, &texture,&line_number);
+            line_offset = target.x();
+            // I should take a slice of u8 and convert it to a str then draw that
             line_offset += 20;
             for j in line_start..line_end+1 {
                 let char = chars[j];
-                let rect = render_char(width as i32, height as i32, char as char);
+                let rect = render_char(letter_width as i32, height as i32, char as char);
                 target.set_x(line_offset as i32);
-                canvas.copy(&texture, rect, target).unwrap();
+                canvas.copy(&texture, rect, *target).unwrap();
                 line_offset += letter_width as i32;
             }
             target.set_y(target.y + height as i32);
+        }
+
+        let mut target = Rect::new(window_width - (letter_width * 4) as i32, 0, letter_width, letter_height);
+        draw_string(&mut canvas, &mut target, &texture,  &fps.to_string());
+        frame_counter += 1;
+        if time_start.elapsed().as_secs() == 1 {
+            fps = frame_counter;
+            frame_counter = 0;
+            time_start = std::time::Instant::now();
         }
 
         canvas.present();
