384833aa4623ba07f5cbca60dd867d6fd2899788
Jimmy Miller <jimmyhmiller@gmail.com>
2022-02-22 12:27:27 -0500

    Changes

--- rust/editor/src/sdl.rs
+++ rust/editor/src/sdl.rs
@@ -1,6 +1,18 @@
-use std::convert::TryInto;
+use std::ptr;
+use std::{convert::TryInto};
+use foreign_types::ForeignType;
+use foreign_types::ForeignTypeRef;
+use metal::CAMetalLayer;
+// use sdl2_sys::*;
+use metal::Device;
+use metal::MTLPixelFormat;
+use metal::MetalLayer;
+use metal::MetalLayerRef;
+use metal::{MetalDrawableRef};
 
-use sdl2::{pixels::Color, render::*, video::{self, WindowContext}, clipboard::ClipboardUtil, mouse::SystemCursor};
+use sdl2::{pixels::Color, render::*, video::{self, WindowContext, Window}, clipboard::ClipboardUtil, mouse::SystemCursor};
+use skia_safe::gpu::mtl;
+use skia_safe::{gpu::{BackendRenderTarget, SurfaceOrigin, DirectContext, mtl::TextureInfo}, ColorType, Surface, ColorSpace};
 
 pub struct SdlContext {
     pub ttf_context: sdl2::ttf::Sdl2TtfContext, 
@@ -31,6 +43,7 @@ pub fn setup_sdl(width: usize, height: usize) -> Result<SdlContext, String> {
         .present_vsync()
         .build()
         .unwrap();
+    
 
 
     let event_pump = sdl_context.event_pump()?;
@@ -71,3 +84,103 @@ pub fn draw_font_texture(texture_creator: &TextureCreator<WindowContext>, ttf_co
     let width = (width / text.len() as u32).try_into().unwrap();
     Ok((texture, width, height.try_into().unwrap()))
 }
+
+
+// fn create_surface(
+//     window_context: &Window,
+//     fb_info: &FramebufferInfo,
+//     gr_context: &mut skia_safe::gpu::DirectContext,
+// ) -> skia_safe::Surface {
+
+//     // let pixel_format = window_context.window_pixel_format();
+//     let size = window_context.size();
+//     let backend_render_target = BackendRenderTarget::new_gl(
+//         (
+//             size.0.try_into().unwrap(),
+//             size.1.try_into().unwrap(),
+//         ),
+//         None,
+//         8,
+//         *fb_info,
+//     );
+//     Surface::from_backend_render_target(
+//         gr_context,
+//         &backend_render_target,
+//         SurfaceOrigin::BottomLeft,
+//         ColorType::RGBA8888,
+//         None,
+//         None,
+//     )
+//     .unwrap()
+// }
+
+fn create_surface(ctx: &mut DirectContext, window: &Window, drawable: &MetalDrawableRef) -> Surface {
+    println!("create_surface start");
+    let draw_size = window.size();
+    let t_info = unsafe { TextureInfo::new(drawable.texture().as_ptr() as *const _) };
+    let target = BackendRenderTarget::new_metal((draw_size.0 as i32, draw_size.1 as i32), 4, &t_info);
+
+    // TODO: let color_type = layer.pixel_format();
+
+    let surface = Surface::from_backend_render_target(
+                                                      ctx,
+                                                      &target,
+                                                      SurfaceOrigin::BottomLeft,
+                                                      ColorType::BGRA8888,
+                                                      ColorSpace::new_srgb(),
+                                                      None,
+    ).unwrap();
+
+    surface
+}
+
+pub fn setup_skia(canvas: &Canvas<Window>) -> skia_safe::Surface {
+    let device = Device::system_default().expect("no device found");
+
+    let layer = unsafe { sdl2_sys::SDL_RenderGetMetalLayer(canvas.raw())} as *mut CAMetalLayer;
+    let layer = unsafe {  MetalLayer::from_ptr(layer) };
+
+	// layer.set_device(&device);
+	// layer.set_pixel_format(MTLPixelFormat::BGRA8Unorm);
+	// layer.set_presents_with_transaction(false);
+	// layer.display_sync_enabled();
+
+	// layer.set_framebuffer_only(false);
+	// layer.set_opaque(true);
+
+	// layer.set_maximum_drawable_count(3);
+
+
+	// let draw_size = window.inner_size();
+	// layer.set_drawable_size(CGSize::new(draw_size.width as f64, draw_size.height as f64));
+
+	let queue = device.new_command_queue();
+
+    let backend = unsafe {
+        mtl::BackendContext::new(
+            device.as_ptr() as mtl::Handle,
+            queue.as_ptr() as mtl::Handle,
+            ptr::null(),
+        )
+    };
+
+    let drawable = layer.next_drawable().unwrap();
+
+	let mut ctx = unsafe {
+		DirectContext::new_metal(&backend, None).expect("Unable to create direct context")
+	};
+    let t_info = unsafe { TextureInfo::new(drawable.texture().as_ptr() as *const _) };
+    let target = BackendRenderTarget::new_metal((canvas.window().size().0 as i32, canvas.window().size().1 as i32), 4, &t_info);
+
+    let surface = Surface::from_backend_render_target(
+        &mut ctx,
+        &target,
+        SurfaceOrigin::BottomLeft,
+        ColorType::BGRA8888,
+            ColorSpace::new_srgb(),
+            None,
+    );
+
+    surface.unwrap()
+}
+
