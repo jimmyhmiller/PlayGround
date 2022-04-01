9a5d71fee53f1a6ba3a51af23c9d91553a452207
Jimmy Miller <jimmyhmiller@gmail.com>
2022-04-01 00:31:09 -0400

    Changes

--- cljs-repl/out/app/main.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.758 {:target :nodejs}
-goog.provide('app.main');
-goog.require('cljs.core');
-app.main.x = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(1),(2),(3)], null);
-cljs.core.println.call(null,"HELLO WORLD!");
-
-//# sourceMappingURL=main.js.map
