1bd114680bb67c66168e9cf6ab50936ff18eac77
Jimmy Miller <jimmyhmiller@gmail.com>
2022-04-01 00:31:09 -0400

    Changes

--- cljs-repl/out/app/main.cljs
+++
@@ -1,5 +0,0 @@
-(ns app.main)
-
-(def x [1 2 3])
-
-(println "HELLO WORLD!")
--- cljs-repl/out/inferred_externs.js
+++
@@ -1,9 +0,0 @@
-var setTimeout;
-var process;
-process.hrtime;
-var Java;
-Java.type;
-Object.out;
-Object.out.println;
-Object.error;
-Object.error.println;
