f50f6e901b7c65acb94c620b9677da845a169b5c
Jimmy Miller <jimmyhmiller@gmail.com>
2022-05-29 17:20:34 -0400

    Changes

--- ruby/visual-two/pages/index.js
+++ ruby/visual-two/pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useRef, useMemo, Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import NoSSR from '@mpth/react-no-ssr';
 import localforage from 'localforage';
@@ -6,17 +6,18 @@ import localforage from 'localforage';
 
 
 // TODO: Make svg cache
+// TODO: get method by name not index
+
 
 // console.log(d3)
 
 // const Graphviz = dynamic(() => import('graphviz-react'), { ssr: false });
 
-const Graphviz = ({id, dot, options: {width, height}, onRender}) => {
+const Graphviz = ({id, dot, options: {width, height}, onRender, cache, setCache}) => {
   let selector = id.replace(/[^A-Za-z\-0-9]/g, "")
   useEffect(() => {
 
-
-    window["@hpcc-js/wasm"].graphviz.layout(dot, "svg", "dot").then(svg => {
+    const renderSvg = (svg) => {
       const div = document.getElementById(selector);
       if (!div) {
         console.error(`no selector ${selector}`)
@@ -26,6 +27,20 @@ const Graphviz = ({id, dot, options: {width, height}, onRender}) => {
       div.children[0].setAttribute("width", `${width}px`)
       div.children[0].setAttribute("height", `${height}px`)
       onRender && onRender()
+    }
+
+    if (cache && cache[selector]) {
+      console.log("cache hit")
+      renderSvg(cache[selector])
+    }
+
+    window["@hpcc-js/wasm"].graphviz.layout(dot, "svg", "dot").then(svg => {
+      renderSvg(svg);
+      setCache && setCache(cache => ({
+        ...cache,
+        [selector]: svg
+      }))
+      
     });
   //   console.log("making graph");
 
@@ -63,14 +78,14 @@ const mod = (m, n) => {
 const useLocalForage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(initialValue);
 
-  useEffect(() => {
-    (async () => {
-      const item = await localforage.getItem(key);
-      if (item) {
-        setStoredValue(item);
-      }
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     const item = await localforage.getItem(key);
+  //     if (item) {
+  //       setStoredValue(item);
+  //     }
+  //   })()
+  // }, [])
 
 
   const setValue = (value) => {
@@ -318,8 +333,9 @@ const partitionBy = (array, f) => {
   if (!array || array.length === 0) {
     return []
   }
-  let results = [[array[0]]];
-  let lastValue = f(array[0]);
+  let first = array.shift();
+  let results = [[first]];
+  let lastValue = f(first);
   let index = 0;
 
   for (let elem of array) {
@@ -357,16 +373,11 @@ const blocksByEpochAccending = (array) => {
 
 
 
-
-const DetailView = ({ blocks, name, setIndex }) => {
-
-  const [rendered, setRendered] = useState(false);
-  const [selected, setSelected] = useState(null);
+const useAddListeners = (setSelected, deps) => {
 
   useEffect(() => {
 
 
-    console.log("adding handlers")
     const titles = Array.from(document.getElementsByTagName("title"))
       .filter(x => x.innerHTML.includes("-"))
       .map(x => x.parentElement);
@@ -379,8 +390,10 @@ const DetailView = ({ blocks, name, setIndex }) => {
           let id = `block-${node.children[0].innerHTML}`;
           let codeList = document.getElementById("codeList").parentElement;
           let blockOffset = document.getElementById(id)?.offsetTop;
+          console.log(blockOffset)
           if (blockOffset) {
-            codeList.scrollTo({top: blockOffset - 70, behavior: 'smooth'})
+            document.getElementById(id).scrollIntoView({behavior: "smooth"})
+            // codeList.scrollTo({top: blockOffset - 140, behavior: 'smooth'})
             setSelected(id)
           }
       }
@@ -389,39 +402,87 @@ const DetailView = ({ blocks, name, setIndex }) => {
     }
 
     return () => {
-      console.log("removing handlers")
       for (let {node, fn} of listeners) {
         node.removeEventListener("mouseover", fn)
       }
     }
 
-  }, [rendered])
+  }, deps)
+}
 
-  const blockGroups = blocksByEpochAccending(blocks);
-  const lastGroup = blockGroups[blockGroups.length - 1];
+const reverse = (arr) => {
+  const temp = [...arr];
+  arr.reverse();
+  return arr;
+}
+
+
+
+const DetailView = ({ blocks, name, setMethod }) => {
+
+  const [rendered, setRendered] = useState(0);
+  const [selected, setSelected] = useState(null);
+  const [index, setIndex] = useState(0);
+  // Epochs are backwards
+  const [epoch, setEpoch] = useState(0)
+  useAddListeners(setSelected, [rendered]);
+
+
+  const blocksByIseq = groupBy(blocks, x => x.iseq_hash);
+  const sortByCount = reverse(sortBy(Object.entries(blocksByIseq), x => x[1].length));
+  const largestIseq = sortByCount[index][1];
+  // console.log(messagesByIseq, sortByCount, largestIseq)
+  const epochs = blocksByEpochAccending(largestIseq);
+
+
+
+
+  const lastGroup = epochs[epochs.length - 1 - epoch] || [];
 
 
   const dot = useMemo(() => makeDot(lastGroup), [lastGroup]);
 
 
   return (
-    <div style={{display: "grid", gridTemplateColumns: "3fr 1fr"}}>
-      <div>
-        <h5 onClick={e => setIndex(null)}>{name}</h5>
-        <Graphviz onRender={() => setRendered(true)} id={`graph-detail-${name}`} dot={dot} options={{width: 1000, height: 1000, fit: true }} />
+
+    <div>
+      <div style={{backgroundColor: "#e8e9eb", height: 60, padding: 10}}>
+        <div style={{backgroundColor: "white", display: "inline-block", marginRight: 10 }}>
+          <h4 onClick={e => setMethod(null)} style={{cursor: "pointer",  padding: 10, margin: 0}}>❮</h4>
+        </div>
+        <div style={{backgroundColor: "white", display: "inline-block", }}>
+          <h4 style={{padding: 10, margin: 0}}>{name}</h4>
+        </div>
       </div>
-      <div style={{height: "99vh", overflow: "scroll"}}>
-        <div id="codeList">
-          {lastGroup.map((block, i) => {
-            const id = `block-${block.hash}-${block.epoch}`;
-            return (
-              <div id={id} key={id} style={{backgroundColor: id === selected ? "#eee" : "white" }}>
-                <pre>
-                  {block.instructions.map(x => `${x.type === "comment" ? "#" : ""} ${x.value}`).join("\n")}
-                </pre>
-              </div>
-            )
-          })}
+      <div style={{display: "grid", gridTemplateColumns: "0.7fr 2.7fr 1fr", marginTop: 30, gap: 10}}>
+        <div style={{ display: "flex", flexDirection: "column", height: "81vh" , overflow: "scroll", padding: 10}}>
+          {sortByCount.map(([k, vals], i) => (
+            <div onClick={e => {setIndex(i); setEpoch(0)}} style={{ backgroundColor: "white", height: 50, textAlign: "center", margin: 10, cursor: "pointer", boxShadow: index === i ? "4px 4px 4px 4px #e8e9eb" : "" }} key={k}>
+              <p>Instruction Sequence {i + 1}</p>
+            </div>
+          ))}
+        </div>
+        <div style={{backgroundColor: "white", padding: 20 }}>
+          <Graphviz onRender={() => setRendered(render => render + 1)} id={`graph-detail-${name}`} dot={dot} options={{width: 1000, height: 750, fit: true }} />
+
+           <input value={epochs.length - 1 - epoch} onChange={e => setEpoch(epochs.length - 1 - e.target.value)} type="range" min="0" max={epochs.length -1} step="1" list="steplist" />
+           <datalist id="steplist">
+             {range(epochs.length - 1).map(x => <option key={x}>x</option>)}
+           </datalist>
+        </div>
+        <div key={index} style={{height: "81vh", overflow: "scroll", marginRight: 20}}>
+          <div  id="codeList">
+            {lastGroup.filter(block => block.instructions.length !== 0).map((block, i) => {
+              const id = `block-${block.hash}-${block.epoch}`;
+              return (
+                <div id={id} key={id} style={{backgroundColor: id === selected ? "#eee" : "white" , padding: "10px 20px 10px 20px", marginBottom: 10}}>
+                  <pre>
+                    {block.instructions.map(x => `${x.type === "comment" ? "#" : ""} ${x.value}`).join("\n")}
+                  </pre>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
@@ -430,21 +491,60 @@ const DetailView = ({ blocks, name, setIndex }) => {
 }
 
 
-const ListView = ({ names, messagesByName, setIndex, setCache }) => {
+const Pill = ({ label, backgroundColor }) => {
+  const pillColor = backgroundColor || "rgb(201 202 204)"
+  return (
+    <p style={{
+        color:"white",
+        marginLeft: 10, 
+        border: `3px solid ${pillColor}`, 
+        backgroundColor: pillColor, 
+        borderRadius: 100, 
+        width:50, 
+        height:25, 
+        textAlign: "center"
+      }} >
+        {label}
+    </p>
+  )
+}
+
+
+
+
+const CountBadges = ({ iseqs }) => {
+  const toTake = iseqs.length > 3 ? 2 : 3;
+  return (
+    <div style={{display:"flex", marginLeft: "auto", marginRight: 20}}>
+      {take(toTake, iseqs).map(([k, values]) => <Pill key={k} label={values.length} />)}
+      {iseqs.length > 3 ? <Pill backgroundColor="rgb(142, 169, 223)" label={`+${iseqs.length - toTake}`} /> : null }
+    </div>
+  )
+}
+
+
+
+const ListView = ({ names, messagesByName, setName, cache, setCache }) => {
 
 
   return (
     <div style={{display: "grid", gridTemplateColumns: "repeat(4, minmax(0, 1fr))", padding: 30,}}>
       {names.map((name, i) => {
+        // TODO: I should just store state this way.
         const messages = messagesByName[name];
-        const epochs = blocksByEpochAccending(messages);
+        const messagesByIseq = groupBy(messages, x => x.iseq_hash);
+        const sortByCount = sortBy(Object.entries(messagesByIseq), x => x[1].length);
+        const largestIseq = sortByCount[sortByCount.length - 1][1];
+        // console.log(messagesByIseq, sortByCount, largestIseq)
+        const epochs = blocksByEpochAccending(largestIseq);
         return (
-          <div style={{margin: 20, boxShadow: "4px 4px 4px 4px #e8e9eb", backgroundColor: "#fff"}} onClick={(e) => setIndex(i)} key={name}>
-            <div style={{borderBottom: "2px solid rgb(232, 233, 235)"}}>
-              <h3 style={{padding: "0px 20px 10px 20px", }}>{messages[0].name}</h3>
+          <div style={{margin: 20, boxShadow: "4px 4px 4px 4px #e8e9eb", backgroundColor: "#fff"}} onClick={(e) => setName(name)} key={name}>
+            <div style={{borderBottom: "2px solid rgb(232, 233, 235)", display: "flex"}}>
+              <h3 style={{padding: "0px 20px 10px 20px", maxWidth: 200}}>{messages[0].name}</h3>
+              <CountBadges iseqs={sortByCount} />
             </div>
             <div style={{padding: 10}}>
-              <Graphviz id={`graph-${name}-${i}`} dot={makeSimplifiedGraph(epochs[epochs.length - 1])} options={{width: 300, height: 300, fit: true }} />
+                <Graphviz cache={cache} setCache={setCache} id={`graph-${name}-${i}`} dot={makeSimplifiedGraph(epochs[epochs.length - 1])} options={{width: 300, height: 300, fit: true }} />
             </div>
           </div>  
         )
@@ -458,7 +558,8 @@ const ListView = ({ names, messagesByName, setIndex, setCache }) => {
 const GraphvizPage = () => {
 
 
-  const [index, setIndex] = useState(null);
+  const [cache, setCache] = useState({});
+  const [name, setName] = useState(null);
   const webSocketMessages = useWebSocket();
   const [savedMessages, setSavedMessages] = useLocalForage("messages", []);
   const messages = savedMessages.concat(webSocketMessages);
@@ -468,13 +569,12 @@ const GraphvizPage = () => {
   const names = Object.keys(messagesByName).filter(x => x !== "");
   names.sort();
 
-  if (index !== null) {
-    const name = names[index];
-    const blocks = messagesByName[names[index]];
-    return <DetailView name={name} blocks={blocks} setIndex={setIndex} />
+  if (name !== null) {
+    const blocks = messagesByName[name];
+    return <DetailView name={name} blocks={blocks} setMethod={setName} />
   }
 
-  return <ListView names={names} messagesByName={messagesByName} setIndex={setIndex} />
+  return <ListView cache={cache} setCache={setCache} names={names} messagesByName={messagesByName} setName={setName} />
   
 }
 
