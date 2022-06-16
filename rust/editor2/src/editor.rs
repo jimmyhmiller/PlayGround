88ca7ec6025fccb0694b9f91a3fbfcd9b685b75e
Jimmy Miller <jimmyhmiller@gmail.com>
2022-06-16 16:42:24 -0400

    Changes

--- rust/editor2/src/editor.rs
+++ rust/editor2/src/editor.rs
@@ -2,21 +2,56 @@
 use crate::fps_counter::FpsCounter;
 
 
-use skia_safe::{RRect, Font, Typeface, FontStyle};
+use skia_safe::{RRect, Font, Typeface, FontStyle, PaintStyle};
 use winit::event::{Event as WinitEvent, WindowEvent as WinitWindowEvent};
 
 
 pub enum Event {
     Noop,
-    MouseMove { x: f64, y: f64 },
-    LeftMouseDown,
-    LeftMouseUp,
-    RightMouseDown,
-    RightMouseUp,
+    MouseMove { x: f32, y: f32 },
+    LeftMouseDown { x: f32, y: f32 },
+    LeftMouseUp { x: f32, y: f32 },
+    RightMouseDown { x: f32, y: f32 },
+    RightMouseUp { x: f32, y: f32 },
     Scroll { x: f64, y: f64 },
+    ClickedWidget { widget_id: usize },
 }
 
+// Need a global store of widgets
+// Then anything else refers to widgets by their id
+// The nice part about that is that widgets can appear in multiple places
+// It might make some other parts more awkward.
+// I can also then deal with events to a widget as data with the id
+// So things can listen for different interactions.
+// If there are a large number of widgets, this could be a bottle neck
+// So I could keep the hierachy. Like I know if the widget is not
+// on the current scene then I am not going to click on it.
+
+
 impl Event {
+
+    fn patch_mouse_event(&mut self, mouse_pos: &Position) {
+        match self {
+            Event::LeftMouseDown { x, y } => {
+                *x = mouse_pos.x;
+                *y = mouse_pos.y;
+            },
+            Event::LeftMouseUp { x, y } => {
+                *x = mouse_pos.x;
+                *y = mouse_pos.y;
+            },
+            Event::RightMouseDown { x, y } => {
+                *x = mouse_pos.x;
+                *y = mouse_pos.y;
+            },
+            Event::RightMouseUp { x, y } => {
+                *x = mouse_pos.x;
+                *y = mouse_pos.y;
+            },
+            _ => {},
+        }
+    }
+
     fn from_winit_event(event: &WinitEvent<'_, ()>) -> Option<Self> {
         match event {
             WinitEvent::WindowEvent { event, .. } => {
@@ -35,16 +70,16 @@ impl Event {
                         use winit::event::MouseButton::*;
                         use winit::event::ElementState::*;
                         match (state, button) {
-                            (Pressed, Left) => Some(Event::LeftMouseDown),
-                            (Released, Left) => Some(Event::LeftMouseUp),
-                            (Pressed, Right) => Some(Event::RightMouseDown),
-                            (Released, Right) => Some(Event::RightMouseUp),
+                            // Silly hack for the fact that I don't know positions here.
+                            (Pressed, Left) => Some(Event::LeftMouseDown { x: -0.0, y: -0.0 }),
+                            (Released, Left) => Some(Event::LeftMouseUp { x: -0.0, y: -0.0 }),
+                            (Pressed, Right) => Some(Event::RightMouseDown { x: -0.0, y: -0.0 }),
+                            (Released, Right) => Some(Event::RightMouseUp { x: -0.0, y: -0.0 }),
                             _ => None,
                         }
 
                     }
-                    // TODO: Cursor moves happen even without moving. Probably need to deduplicate somewhere
-                    CursorMoved { position, .. }  => Some(Event::MouseMove { x: position.x, y: position.y }),
+                    CursorMoved { position, .. }  => Some(Event::MouseMove { x: position.x as f32, y: position.y as f32 }),
                     _ => {
                         println!("Unhandled event: {:?}", event);
                         None
@@ -92,6 +127,16 @@ impl Widget {
             }
         }
     }
+
+    fn mouse_over(&self, position: &Position) -> bool {
+        let x = position.x;
+        let y = position.y;
+        let x_min = self.position.x;
+        let x_max = self.position.x + self.size.width;
+        let y_min = self.position.y;
+        let y_max = self.position.y + self.size.height;
+        x >= x_min && x <= x_max && y >= y_min && y <= y_max
+    }
 }
 
 struct Widget {
@@ -107,13 +152,22 @@ struct Scene {
     widgets: Vec<Widget>,
 }
 
+struct Context {
+    mouse_position: Position,
+    left_mouse_down: bool,
+    right_mouse_down: bool,
+}
+
 
 pub struct Editor {
     pub events: Vec<Event>,
     pub fps_counter: FpsCounter,
     scenes: Vec<Scene>,
     current_scene: usize,
+    context: Context,
+    scene_selector: Vec<Widget>,
 }
+
 
 
 #[cfg(all(target_os = "macos"))]
@@ -159,6 +213,18 @@ impl<'a> Editor {
                 }
             ]
         });
+
+
+        for (i, _scene) in self.scenes.iter().enumerate() {
+
+            self.scene_selector.push(Widget {
+                position: Position { x: 20.0, y: i as f32 * 120.0 + 20.0 },
+                size: Size { width: 100.0, height: 100.0 },
+                data: WidgetData::Noop,
+            });
+            
+        }
+
     }
 
     pub fn update(&mut self) {
@@ -171,15 +237,19 @@ impl<'a> Editor {
             fps_counter: FpsCounter::new(),
             scenes: vec![],
             current_scene: 0,
+            context: Context {
+                mouse_position: Position { x: 0.0, y: 0.0 },
+                left_mouse_down: false,
+                right_mouse_down: false,
+            },
+            scene_selector: vec![],
         }
     }
 
-            // let white = &Paint::new(Color4f::new(1.0, 1.0, 1.0, 1.0), None);
-
-        // let purple = parse_hex("#1c041e");
-        // let yellow = parse_hex("#2c1805");
-        // let green = parse_hex("#011b1e");
-        
+    // let purple = parse_hex("#1c041e");
+    // let yellow = parse_hex("#2c1805");
+    // let green = parse_hex("#011b1e");
+    
 
     pub fn draw(&mut self, canvas: &mut Canvas) {  
         
@@ -203,12 +273,29 @@ impl<'a> Editor {
         // a widget if the mouse is over it?
         // Or do I want the widget to look at the mouse position?
         // Maybe allow for both?
-        for (i, _scene) in self.scenes.iter().enumerate() {
-            let rect = Rect::from_xywh(20.0, i as f32 * 120.0 + 20.0, 100.0, 100.0);
+        for widget in self.scene_selector.iter() {
+            let rect = Rect::from_xywh(widget.position.x, widget.position.y, widget.size.width, widget.size.height);
             let rrect = RRect::new_rect_xy(rect, 20.0, 20.0);
             let purple = parse_hex("#1c041e");
-            let green = parse_hex("#011b1e");
-            canvas.draw_rrect(rrect, if i == self.current_scene { &green } else { &purple });
+           
+            if widget.mouse_over(&self.context.mouse_position) {
+                let mut outline = white.clone();
+                // green_outline.set_stroke(Some(2.0));
+                outline.set_style(PaintStyle::Stroke);
+                outline.set_stroke_width(3.0);
+                outline.set_anti_alias(true);
+                canvas.draw_rrect(rrect, &outline);
+                let rect = Rect::from_xywh(
+                    widget.position.x + 5.0,
+                    widget.position.y + 5.0,
+                    widget.size.width - 10.0,
+                    widget.size.height - 10.0
+                );
+                let rrect = RRect::new_rect_xy(rect, 15.0, 15.0);
+                canvas.draw_rrect(rrect, &purple);
+            } else {
+                canvas.draw_rrect(rrect, &purple);
+            }
         }
 
         let scene = &self.scenes[self.current_scene];
@@ -240,20 +327,47 @@ impl<'a> Editor {
     }
 
     pub fn add_event(&mut self, event: &winit::event::Event<'_, ()>) {
-        if let Some(event) = Event::from_winit_event(&event) {
+        if let Some(mut event) = Event::from_winit_event(&event) {
+            event.patch_mouse_event(&self.context.mouse_position);
             match event {
+                // Should this even happen?
+                Event::ClickedWidget { widget_id: _ } => {
+                    println!("Clicked widget");
+                }
                 Event::Noop => {},
-                Event::MouseMove { x: _, y: _ } => {},
-                Event::LeftMouseDown => {},
-                Event::LeftMouseUp => {},
-                Event::RightMouseDown => {},
-                Event::RightMouseUp => {},
+                Event::MouseMove { x, y } => {
+                    // Not pushing the event because there are too many
+                    self.context.mouse_position = Position { x, y };
+                },
+                Event::LeftMouseDown {..} => {
+                    self.events.push(event);
+                    self.context.left_mouse_down = true;
+                },
+                Event::LeftMouseUp {..} => {
+                    self.events.push(event);
+                    self.context.left_mouse_down = false;
+                    // Probably not the right place.
+                    // Maybe need events on last cycle?
+
+                    self.add_clicks();
+                },
+                Event::RightMouseDown {..} => {
+                    self.events.push(event);
+                    self.context.right_mouse_down = true;
+                },
+                Event::RightMouseUp {..} => {
+                    self.events.push(event);
+                    self.context.right_mouse_down = false;
+                },
                 Event::Scroll { x: _, y: _ } => {
-                
+                    self.events.push(event);
                 }
             }
-            self.events.push(event);
         }
+    }
+
+    fn add_clicks(&mut self) -> () {
+      
     }
 }
 
