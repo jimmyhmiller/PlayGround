8baffe5355fecea65c187c7ff85e63c3c2f7d51c
Jimmy Miller <jimmy.miller@shopify.com>
2023-01-07 12:00:18 -0500

    Changes

--- rust/extension-system/my-first-extension/src/lib.rs
+++ rust/extension-system/my-first-extension/src/lib.rs
@@ -1,90 +1,38 @@
-// #[no_mangle]
-// pub extern "C" fn answer(s: &MyStruct) -> i32 {
-//     println!("a {} b {}", s.a, s.b);
-//     (unsafe { foo(*s) }) + 42
-// }
+use framework::{App, Canvas};
+mod framework;
 
-// #[repr(C)]
-// #[derive(Copy, Clone)]
-// pub struct MyStruct {
-//     pub a: i32,
-//     pub b: i32,
-// }
-
-
-// #[link(wasm_import_module = "host")]
-// extern "C" {
-//     fn foo(s: MyStruct) -> i32;
-// }
-
-static mut STATE: isize = 0;
-
-#[link(wasm_import_module = "host")]
-extern "C" {
-    // imports the name `foo` from `the-wasm-import-module`
-    fn draw_rect(x: f32, y: f32, width: f32, height: f32);
+struct Counter {
+    count: isize,
 }
 
-#[link(wasm_import_module = "host")]
-extern "C" {
-    #[link_name = "draw_str"]
-    fn draw_str_low_level(ptr: i32, len: i32, x: f32, y: f32);
-}
+impl App for Counter {
+    type State = isize;
 
-
-
-
-fn draw_str(s: &str, x: f32, y: f32) {
-    unsafe {
-        draw_str_low_level(s.as_ptr() as i32, s.len() as i32, x, y);
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn on_click() {
-    unsafe { STATE += 1; }
-}
-
-#[repr(C)]
-pub struct PointerLengthString {
-    pub ptr: usize,
-    pub len: usize,
-}
-
-impl From<String> for PointerLengthString {
-    fn from(s: String) -> Self {
+    fn init() -> Self {
         Self {
-            ptr: s.as_ptr() as usize,
-            len: s.len(),
+            count: 0,
         }
     }
-}
 
-impl From<PointerLengthString> for String {
-    fn from(s: PointerLengthString) -> Self {
-        unsafe { String::from_raw_parts(s.ptr as *mut u8, s.len, s.len) }
+    fn draw(&mut self) {
+        let canvas = Canvas::new();
+        canvas.draw_rect(0.0, 0.0, 300 as f32, 100 as f32);
+        canvas.draw_str(&format!("Count: {}", self.count), 40.0, 50.0);
+    }
+
+    fn on_click(&mut self) {
+        self.count += 1;
+    }
+
+    fn get_state(&self) -> Self::State {
+        self.count
+    }
+
+    fn set_state(&mut self, state: Self::State) {
+        self.count = state;
     }
 }
 
-#[no_mangle]
-pub extern "C" fn get_state() -> *const PointerLengthString {
-    let s = serde_json::to_string(unsafe { &STATE }).unwrap();
-    let p : PointerLengthString = s.into();
-    &p as *const _
-}
 
-#[no_mangle]
-pub extern "C" fn set_state(ptr: i32, len: i32) {
-    let s = String::from(PointerLengthString { ptr: ptr as usize, len: len as usize });
-    let state: isize = serde_json::from_str(&s).unwrap();
-    unsafe { STATE = state; }
-}
+app!(Counter);
 
-#[no_mangle]
-pub extern "C" fn draw() {
-
-    unsafe {
-        draw_rect(0.0, 0.0, 300 as f32, 100 as f32);
-        draw_str(&format!("Count: {}", STATE), 40.0, 50.0);
-    }
-}
