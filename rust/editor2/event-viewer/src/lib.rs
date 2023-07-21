7e0d0c04b7e2dd91760a5c9b464fd7c6d42c26e5
Jimmy Miller <jimmyhmiller@gmail.com>
2023-07-21 14:43:16 -0400

    Changes

--- rust/editor2/event-viewer/src/lib.rs
+++ rust/editor2/event-viewer/src/lib.rs
@@ -1,5 +1,5 @@
-use framework::{App, app, Size, Canvas, Color, Rect};
-use serde::{Serialize, Deserialize};
+use framework::{app, App, Canvas, Color, Rect, Size};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct Event {
@@ -12,6 +12,72 @@ struct EventViewer {
     size: Size,
     events: Vec<Event>,
     y_scroll_offset: f32,
+}
+
+struct Ui {}
+enum Component {
+    Pane(Size, Box<Component>),
+    List(Vec<Component>),
+    Container(Box<Component>),
+    Text(String),
+}
+
+impl Component {
+    fn draw(&self, canvas: &mut Canvas) {
+        match self {
+            Component::Pane(size, child) => {
+                let background = Color::parse_hex("#353f38");
+
+                let bounding_rect = Rect::new(0.0, 0.0, size.width, size.height);
+
+                canvas.save();
+                canvas.set_color(&background);
+                canvas.clip_rect(bounding_rect);
+                canvas.draw_rrect(bounding_rect, 20.0);
+
+                canvas.clip_rect(bounding_rect.with_inset((20.0, 20.0)));
+                child.draw(canvas);
+            }
+            Component::List(children) => {
+                for child in children.iter() {
+                    child.draw(canvas);
+                    // TODO: need to translate based on height of component
+                    canvas.translate(0.0, 30.0);
+                }
+            }
+            Component::Container(child) => {
+                child.draw(canvas);
+            }
+            Component::Text(text) => {
+                canvas.save();
+                canvas.set_color(&Color::parse_hex("#ffffff"));
+                canvas.draw_str(text, 40.0, 0.0);
+                canvas.restore();
+            }
+        }
+    }
+}
+
+impl Ui {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn pane(&self, size: Size, child: Component) -> Component {
+        Component::Pane(size, Box::new(child))
+    }
+
+    fn list(&self, events: &[Event], f: impl Fn(&Self, &Event) -> Component) -> Component {
+        Component::List(events.iter().map(|event| f(self, event)).collect())
+    }
+
+    fn container(&self, child: Component) -> Component {
+        Component::Container(Box::new(child))
+    }
+
+    fn text(&self, text: &str) -> Component {
+        Component::Text(text.to_string())
+    }
 }
 
 impl App for EventViewer {
@@ -28,46 +94,22 @@ impl App for EventViewer {
     }
 
     fn draw(&mut self) {
+
         let mut canvas = Canvas::new();
-        let background = Color::parse_hex("#353f38");
 
-        let bounding_rect = Rect::new(
-            0.0,
-            0.0,
-            self.size.width,
-            self.size.height,
+        let ui = Ui::new();
+        let ui = ui.pane(
+            self.size,
+            ui.list(&self.events, |ui, event|
+                ui.container(ui.text(&event.kind)
+            )),
         );
-
-        canvas.save();
-        canvas.set_color(&background);
-        canvas.clip_rect(bounding_rect);
-        canvas.draw_rrect(bounding_rect, 20.0);
-
-        canvas.clip_rect(bounding_rect.with_inset((20.0, 20.0)));
-
-        canvas.set_color(&Color::parse_hex("#ffffff"));
-        
-        // We should really by default have a better
-        // starting point for text and stuff.
-        // I mean actually we probably just need to have a layout manager.
-        canvas.translate(0.0, self.y_scroll_offset);
-
-        canvas.translate(0.0, 50.0);
-
-        for event in self.events.iter() {
-            // println!("{:?}", event);
-            canvas.draw_str(&event.kind, 40.0, 0.0);
-            canvas.translate(0.0, 30.0)
-        }
+        ui.draw(&mut canvas);
     }
 
-    fn on_click(&mut self, _x: f32, _y: f32) {
-        
-    }
+    fn on_click(&mut self, _x: f32, _y: f32) {}
 
-    fn on_key(&mut self, _input: framework::KeyboardInput) {
-    }
-
+    fn on_key(&mut self, _input: framework::KeyboardInput) {}
 
     // TODO: It would be nice to have scroll work for free
     // by default and only need to deal with overriding it.
@@ -94,7 +136,6 @@ impl App for EventViewer {
     fn on_event(&mut self, kind: String, event: String) {
         self.events.push(Event { kind, event });
     }
-
 }
 
 app!(EventViewer);
