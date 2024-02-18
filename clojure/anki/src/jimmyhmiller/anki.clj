f24b3c7409cb96cf8b01f5f649ea5994e5bef3f9
Jimmy Miller <jimmyhmiller@gmail.com>
2024-02-18 12:02:43 -0500

    Changes

--- clojure/anki/src/jimmyhmiller/anki.clj
+++ clojure/anki/src/jimmyhmiller/anki.clj
@@ -1,12 +1,16 @@
 (ns jimmyhmiller.anki
-  (:require [clj-anki.core :as anki]))
+  (:require [clj-anki.core :as anki]
+            [clj-chess.pgn :as pgn]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+
+(pgn/games-in-file  "/Users/jimmyhmiller/Downloads/lichess_study_london-system-gotham-chess-style_by_xGROM_2021.01.26.pgn")
+
+
+(def gotham-london-study (pgn/parse-pgn (slurp "/Users/jimmyhmiller/Downloads/lichess_study_london-system-gotham-chess-style_by_xGROM_2021.01.26.pgn")))
+
+(def notes (anki/read-notes "/Users/jimmyhmiller/Downloads/Chess_Openings.apkg"))
+
+(def london (anki/read-notes "/Users/jimmyhmiller/Downloads/London system.apkg"))
+
+(first london)
