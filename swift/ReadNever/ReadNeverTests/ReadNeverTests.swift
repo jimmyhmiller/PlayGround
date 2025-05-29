1909d1c06b7222b0c08437a651eebf4ceaca0b7a
Jimmy Miller <jimmyhmiller@gmail.com>
2025-05-29 19:29:36 -0400

    Changes

---
+++ swift/ReadNever/ReadNeverTests/ReadNeverTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadNeverTests.swift
+//  ReadNeverTests
+//
+//  Created by Jimmy Miller on 5/25/25.
+//
+
+import Testing
+@testable import ReadNever
+
+struct ReadNeverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
